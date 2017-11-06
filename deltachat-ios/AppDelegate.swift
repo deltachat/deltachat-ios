b716eccb78dde9846dc36a5e19a973fabcda697f
Jonas Reinsch <jonas.reinsch@gmail.com>
2017-11-06 10:30:09 +0100

    remove storyboard

--- deltachat-ios/AppDelegate.swift
+++ deltachat-ios/AppDelegate.swift
@@ -16,6 +16,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+
+        window = UIWindow(frame: UIScreen.main.bounds)
+        guard let window = window else {
+            fatalError("window was nil in app delegate")
+        }
+        window.rootViewController = ViewController()
+        window.makeKeyAndVisible()
+        window.backgroundColor = UIColor.white
         return true
     }
 
