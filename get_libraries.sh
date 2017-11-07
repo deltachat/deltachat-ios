d8875b7d52caae5a2e8f5735219ea2a12eb280e3
Jonas Reinsch <jonas.reinsch@gmail.com>
2017-11-07 17:24:50 +0100

    modify get_libraries script so that only deltachat-core subdirectory is deleted and libetpan is left alone

--- get_libraries.sh
+++ get_libraries.sh
@@ -2,14 +2,13 @@
 
 base='../deltachat-core'
 dst=deltachat-ios/libraries
-# dst_deltachat_core="${dst}/deltachat-core"
 
 if [[ ! -d "${base}" ]]; then
     echo Error: deltachat-core repository expected in parent directory
     exit 1
 fi
 
-rm -rf "${dst}"
+rm -rf "${dst}"/deltachat-core
 mkdir -p "${dst}"
 cp -r "${base}" "$dst"
 rm -rf "$dst"/deltachat-core/.git*
