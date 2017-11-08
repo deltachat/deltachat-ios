19639cecf3880b80843a7c5fb492fa2d18784493
Bastian van de Wetering <nayooti@gmail.com>
2017-11-08 12:35:52 +0100

    Added Message.swift -> struct Message

---
+++ deltachat-ios/Message.swift
@@ -0,0 +1,26 @@
+//
+//  Message.swift
+//  deltachat-ios
+//
+//  Created by Bastian van de Wetering on 08.11.17.
+//  Copyright © 2017 Jonas Reinsch. All rights reserved.
+//
+
+import Foundation
+import MessageKit
+
+struct Message: MessageType {
+    
+    var messageId: String
+    var sender: Sender
+    var sentDate: Date
+    var data: MessageData
+    
+    init(data: MessageData, sender: Sender, messageId: String, date: Date) {
+        self.data = data
+        self.sender = sender
+        self.messageId = messageId
+        self.sentDate = date
+    }
+    
+}
