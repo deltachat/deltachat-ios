8b37ae3c12706e4ac3700eaf7eeb09c9fb330dd7
Alla Reinsch <reinsch.alla@gmail.com>
2018-07-18 22:38:13 +0200

    remove unneeded code

--- deltachat-ios/NewChatViewController.swift
+++ deltachat-ios/NewChatViewController.swift
@@ -85,7 +85,6 @@ class NewChatViewController: UITableViewController {
         if let c = tableView.dequeueReusableCell(withIdentifier: "contactCell") as? ContactCell {
             cell = c
         } else {
-            // cell = UITableViewCell(style: .value1, reuseIdentifier: "contactCell")
             cell = ContactCell(style: .default, reuseIdentifier: "contactCell")
         }
         
@@ -133,51 +132,4 @@ class NewChatViewController: UITableViewController {
             navigationController?.pushViewController(contactProfileController, animated: true)
         }
     }
-
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
