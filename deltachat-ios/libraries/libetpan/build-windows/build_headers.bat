2fc811d992510eeedb95b2e95deb7504ec741458
dignifiedquire <dignifiedquire@users.noreply.github.com>
2018-12-24 23:46:18 +0100

    remove hard dependency on libetpan

--- deltachat-ios/libraries/libetpan/build-windows/build_headers.bat
+++
@@ -1,4 +0,0 @@
-@mkdir include
-@mkdir include\libetpan
-@for /F "delims=" %%a in (build_headers.list) do @copy "..\%%a" include\libetpan
-@echo "done" >_headers_depends
--- deltachat-ios/libraries/libetpan/travis/before-script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -e
-
-if test "x`uname`" = xLinux ; then
-  sudo apt-get install libsasl2-dev libssl-dev zlib1g-dev
-fi
