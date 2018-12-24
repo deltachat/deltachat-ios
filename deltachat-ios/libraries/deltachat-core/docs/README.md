b7b8e7c4ad3fc7717591ec3f87fd87247d28192a
dignifiedquire <dignifiedquire@users.noreply.github.com>
2018-12-24 23:44:01 +0100

    remove hard dep on deltachat-core

--- deltachat-ios/libraries/deltachat-core/docs/README.md
+++
@@ -1,4 +0,0 @@
-to get the api documentation,
-
-- call doxygen in this directory and browse the `html` subdirectory
-- or read it online at <https://c.delta.chat>
--- deltachat-ios/libraries/deltachat-core/src/dc_keyhistory.c
+++
@@ -1,8 +0,0 @@
-#include "dc_context.h"
-
-
-void dc_add_to_keyhistory(dc_context_t* context, const char* rfc724_mid, time_t sending_time, const char* addr, const char* fingerprint)
-{
-	// TODO
-}
-
