bbade3e436f9bcede59a3a34e7f3f83da79b11a7
Bastian van de Wetering <nayooti@gmail.com>
2019-04-02 17:24:19 +0200

    tap on passwordfield will start oAuth if needed

--- deltachat-ios/TextFieldCell.swift
+++ deltachat-ios/TextFieldCell.swift
@@ -21,19 +21,23 @@ class TextFieldCell: UITableViewCell {
         return textField
     }()
 
-  init(description: String, placeholder: String) {
-    self.placeholder = placeholder
+    init(description: String, placeholder: String) {
+        self.placeholder = placeholder
 
-    super.init(style: .value1, reuseIdentifier: nil)
+        super.init(style: .value1, reuseIdentifier: nil)
 
-    textLabel?.text = "\(description):"
+        textLabel?.text = "\(description):"
 
-    // see: https://stackoverflow.com/a/35903650
-    // this makes the textField respect the trailing margin of
-    // the table view cell
-    selectionStyle = .none
-    setupViews()
-  }
+        // see: https://stackoverflow.com/a/35903650
+        // this makes the textField respect the trailing margin of
+        // the table view cell
+        selectionStyle = .none
+        setupViews()
+    }
+
+    required init?(coder _: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     private func setupViews() {
         contentView.addSubview(textField)
@@ -43,65 +47,67 @@ class TextFieldCell: UITableViewCell {
         textField.trailingAnchor.constraint(equalTo: trailing).isActive = true
         textField.centerYAnchor.constraint(equalTo: contentView.centerYAnchor).isActive = true
         if let label = self.textLabel {
-            textField.leadingAnchor.constraint(equalTo: label.trailingAnchor, constant: 20).isActive = true
+            textField.leadingAnchor.constraint(equalTo: label.trailingAnchor, constant: 20).isActive = true // this will prevent the textfield from growing over the textLabel while typing
         } else {
             textField.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20).isActive = true
         }
     }
 
-  override func setSelected(_ selected: Bool, animated _: Bool) {
-    if selected {
-      textField.becomeFirstResponder()
+    override func setSelected(_ selected: Bool, animated _: Bool) {
+        if selected {
+            textField.becomeFirstResponder()
+        }
     }
-  }
 
-  required init?(coder _: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
-  }
+    func getText() -> String? {
+        return self.textField.text 
+    }
 
-  static func makeEmailCell() -> TextFieldCell {
-    let emailCell = TextFieldCell(description: "Email", placeholder: "you@example.com")
 
-    emailCell.textField.keyboardType = .emailAddress
-    // switch off quicktype
-    emailCell.textField.autocorrectionType = .no
-    emailCell.textField.autocapitalizationType = .none
 
-    return emailCell
-  }
+    static func makeEmailCell() -> TextFieldCell {
+        let emailCell = TextFieldCell(description: "Email", placeholder: "you@example.com")
 
-  static func makePasswordCell() -> TextFieldCell {
-    let passwordCell = TextFieldCell(description: "Password", placeholder: "your IMAP password")
+        emailCell.textField.keyboardType = .emailAddress
+        // switch off quicktype
+        emailCell.textField.autocorrectionType = .no
+        emailCell.textField.autocapitalizationType = .none
 
-    passwordCell.textField.textContentType = UITextContentType.password
-    passwordCell.textField.isSecureTextEntry = true
+        return emailCell
+    }
 
-    return passwordCell
-  }
+    static func makePasswordCell() -> TextFieldCell {
+        let passwordCell = TextFieldCell(description: "Password", placeholder: "your IMAP password")
 
-  static func makeNameCell() -> TextFieldCell {
-    let nameCell = TextFieldCell(description: "Name", placeholder: "new contacts nickname")
+        passwordCell.textField.textContentType = UITextContentType.password
+        passwordCell.textField.isSecureTextEntry = true
 
-    nameCell.textField.autocapitalizationType = .words
-    nameCell.textField.autocorrectionType = .no
-    // .namePhonePad doesn't support autocapitalization
-    // see: https://stackoverflow.com/a/36365399
-    // therefore we use .default to capitalize the first character of the name
-    nameCell.textField.keyboardType = .default
+        return passwordCell
+    }
 
-    return nameCell
-  }
+    static func makeNameCell() -> TextFieldCell {
+        let nameCell = TextFieldCell(description: "Name", placeholder: "new contacts nickname")
 
-  static func makeConfigCell(label: String, placeholder: String) -> TextFieldCell {
-    let nameCell = TextFieldCell(description: label, placeholder: placeholder)
+        nameCell.textField.autocapitalizationType = .words
+        nameCell.textField.autocorrectionType = .no
+        // .namePhonePad doesn't support autocapitalization
+        // see: https://stackoverflow.com/a/36365399
+        // therefore we use .default to capitalize the first character of the name
+        nameCell.textField.keyboardType = .default
 
-    nameCell.textField.autocapitalizationType = .words
-    nameCell.textField.autocorrectionType = .no
-    // .namePhonePad doesn't support autocapitalization
-    // see: https://stackoverflow.com/a/36365399
-    // therefore we use .default to capitalize the first character of the name
-    nameCell.textField.keyboardType = .default
+        return nameCell
+    }
 
-    return nameCell
-  }
+    static func makeConfigCell(label: String, placeholder: String) -> TextFieldCell {
+        let nameCell = TextFieldCell(description: label, placeholder: placeholder)
+
+        nameCell.textField.autocapitalizationType = .words
+        nameCell.textField.autocorrectionType = .no
+        // .namePhonePad doesn't support autocapitalization
+        // see: https://stackoverflow.com/a/36365399
+        // therefore we use .default to capitalize the first character of the name
+        nameCell.textField.keyboardType = .default
+
+        return nameCell
+    }
 }
