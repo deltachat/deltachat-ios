ddec3992c36e25daaef7c8393fabbebae0c75df0
Bastian van de Wetering <nayooti@gmail.com>
2019-04-10 12:53:19 +0200

    added urlSession extension to run synchronous data tasks - used in DC_EVENT_HTTP_POST-event callback

--- deltachat-ios/Extensions/Extensions.swift
+++ deltachat-ios/Extensions/Extensions.swift
@@ -55,3 +55,26 @@ extension CharacterSet {
 		return allowed
 	}()
 }
+
+extension URLSession {
+	func synchronousDataTask(with url: URL) -> (Data?, URLResponse?, Error?) {
+		var data: Data?
+		var response: URLResponse?
+		var error: Error?
+
+		let semaphore = DispatchSemaphore(value: 0)
+
+		let dataTask = self.dataTask(with: url) {
+			data = $0
+			response = $1
+			error = $2
+
+			semaphore.signal()
+		}
+		dataTask.resume()
+
+		_ = semaphore.wait(timeout: .distantFuture)
+
+		return (data, response, error)
+	}
+}
