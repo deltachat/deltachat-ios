60f122c0e87f601e34f5f425c4b06d7e92321028
Bastian van de Wetering <nayooti@gmail.com>
2019-04-17 12:34:46 +0200

    removed restore option from settings controller - removed close button from accont setup - when opening account setup from settings, account setup will be nav-pushed (not presented)

--- deltachat-ios/TopViews/SettingsController.swift
+++ deltachat-ios/TopViews/SettingsController.swift
@@ -280,6 +280,6 @@ internal final class SettingsViewController: QuickTableViewController {
   private func presentAccountSetup(_: Row) {
 		if let nav = self.navigationController {
 			nav.pushViewController(AccountSetupController(), animated: true)
-		}
+		}	
   }
 }
