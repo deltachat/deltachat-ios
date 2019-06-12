e15589b881c0fb8c016e33f3fba4b48c1f47ef95
Bastian van de Wetering <nayooti@gmail.com>
2019-06-12 10:31:55 +0200

    bugfix - footer in accountSetup remained visible under certain circumstances

--- deltachat-ios/Coordinator/AppCoordinator.swift
+++ deltachat-ios/Coordinator/AppCoordinator.swift
@@ -422,10 +422,6 @@ class ChatViewCoordinator: Coordinator {
 						let pixelSize = compressedImage.imageSizeInPixel()
 						let width = Int32(exactly: pixelSize.width)!
 						let height =  Int32(exactly: pixelSize.height)!
-//						let width = Int32(exactly: compressedImage.size.width)!
-//						let height = Int32(exactly: compressedImage.size.height)!
-						print("CompressImage width: \(width)")
-						print("CompressImage height: \(height)")
 						let path = Utils.saveImage(image: compressedImage)
 						let msg = dc_msg_new(mailboxPointer, DC_MSG_IMAGE)
 						dc_msg_set_file(msg, path, "image/jpeg")
