9a6d8c8851fdf1fb783758cb247f4d0d2da62b8e
B. Petersen <r10s@b44t.com>
2019-09-22 02:06:51 +0200

    adapt tools/add-language.sh script

--- tools/add-language.sh
+++ tools/add-language.sh
@@ -1,19 +1,7 @@
 # add a language, must be executed from the repo root
 
-if [ $# -eq 0 ]
-then
-  echo "Please specify the language to add as the first argument (dk, ru, pt-BR etc.)"
-  exit
-fi
-
-LANG=$1
-
-mkdir ./deltachat-ios/$LANG.lproj/
-
-cp ./deltachat-ios/en.lproj/Localizable.strings ./deltachat-ios/$LANG.lproj/
-cp ./deltachat-ios/en.lproj/Localizable.stringsdict ./deltachat-ios/$LANG.lproj/
-cp ./deltachat-ios/en.lproj/Untranslated.stringsdict ./deltachat-ios/$LANG.lproj/
-
-echo "res/values-$LANG/strings.xml added:"
+echo "to add a language, browse in Xcode to "
+echo "Project settings > Info > Localisations "
+echo "and add the desired lanugage, then:"
 echo "- if needed, language mappings can be added to tools/.tx/config"
 echo "- then, call ./tools/tx-pull-translations"
