67ca117130879577c3d17981036f94951d0ba56f
B. Petersen <r10s@b44t.com>
2019-09-25 22:31:13 +0200

    add navigation-control to profile

--- deltachat-ios/Controller/EditSettingsController.swift
+++ deltachat-ios/Controller/EditSettingsController.swift
@@ -10,7 +10,12 @@ class EditSettingsController: UITableViewController {
     private let section1Name = 0
     private let section1Status = 1
     private let section1RowCount = 2
-    private let sectionCount = 1
+
+    private let section2 = 1
+    private let section2AccountSettings = 0
+    private let section2RowCount = 1
+
+    private let sectionCount = 2
 
     private lazy var displayNameCell: TextFieldCell = {
         let cell = TextFieldCell(description: String.localized("pref_your_name"), placeholder: String.localized("pref_your_name"))
@@ -21,6 +26,14 @@ class EditSettingsController: UITableViewController {
     private lazy var statusCell: TextFieldCell = {
         let cell = TextFieldCell(description: String.localized("pref_default_status_label"), placeholder: String.localized("pref_default_status_label"))
         cell.setText(text: DcConfig.selfstatus ?? nil)
+        return cell
+    }()
+
+    lazy var accountSettingsCell: UITableViewCell = {
+        let cell = UITableViewCell(style: .value1, reuseIdentifier: nil)
+        cell.textLabel?.text = String.localized("pref_password_and_account_settings")
+        cell.accessoryType = .disclosureIndicator
+        cell.accessibilityIdentifier = "accountSettingsCell"
         return cell
     }()
 
@@ -58,14 +71,23 @@ class EditSettingsController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return section1RowCount
+        if section == section1 {
+            return section1RowCount
+        } else {
+            return section2RowCount
+        }
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if indexPath.section == section1 && indexPath.row == section1Name {
-            return displayNameCell
+        if indexPath.section == section1 {
+            if indexPath.row == section1Name {
+                return displayNameCell
+            } else {
+                return statusCell
+            }
+        } else {
+            return accountSettingsCell
         }
-        return statusCell
     }
 
     override func tableView(_: UITableView, titleForFooterInSection section: Int) -> String? {
@@ -73,6 +95,12 @@ class EditSettingsController: UITableViewController {
             return String.localized("pref_who_can_see_profile_explain")
         } else {
             return nil
+        }
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let cell = tableView.cellForRow(at: indexPath) else { return }
+        if cell.accessibilityIdentifier == "accountSettingsCell" {
         }
     }
 }
