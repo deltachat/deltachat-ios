a041f34df6dd4f0ee8c004dfb44c06d646c17268
B. Petersen <r10s@b44t.com>
2019-10-06 15:21:23 +0200

    keep a strong reference to the coordinator when opening account-settings

--- deltachat-ios/Controller/EditSettingsController.swift
+++ deltachat-ios/Controller/EditSettingsController.swift
@@ -17,6 +17,8 @@ class EditSettingsController: UITableViewController {
 
     private let sectionCount = 2
 
+    private var childCoordinators: Coordinator?
+
     private lazy var displayNameCell: TextFieldCell = {
         let cell = TextFieldCell(description: String.localized("pref_your_name"), placeholder: String.localized("pref_your_name"))
         cell.setText(text: DcConfig.displayname ?? nil)
@@ -106,6 +108,7 @@ class EditSettingsController: UITableViewController {
             guard let nc = navigationController else { return }
             let accountSetupVC = AccountSetupController(dcContext: dcContext, editView: true)
             let coordinator = AccountSetupCoordinator(dcContext: dcContext, navigationController: nc)
+            self.childCoordinators = coordinator
             accountSetupVC.coordinator = coordinator
             nc.pushViewController(accountSetupVC, animated: true)
         }
