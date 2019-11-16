8dcf1eb5de1ee5e084e604849941b1f731e7494f
cyberta <cyberta@riseup.net>
2019-11-16 13:23:21 +0100

    implement helper to save selfavatar in app-local document storage

---
+++ deltachat-ios/Helper/AvatarHelper.swift
@@ -0,0 +1,56 @@
+//
+//  AvatarHelper.swift
+//  deltachat-ios
+//
+//  Created by Macci on 15.11.19.
+//  Copyright © 2019 Jonas Reinsch. All rights reserved.
+//
+
+import Foundation
+
+
+class AvatarHelper {
+
+    static let groupTemplate = "group_chat_avatar_%s_.jpg"
+    static let contactTemplate = "contact_avatar_%s.jpg"
+    static let selfAvatarFile = "contact_avatar_self.jpg"
+    private static let avatarPath = "avatars"
+
+    // blocking method (reads/writes to storage)
+    static func setSelfAvatarFile(fileUrl: URL) {
+        let filemanager = FileManager.default
+        let docDir = filemanager.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        let avatarDir = docDir.appendingPathComponent(avatarPath)
+        let avatarFile = avatarDir.appendingPathComponent(selfAvatarFile)
+        do {
+            try filemanager.createDirectory(atPath: avatarDir.path,
+                                            withIntermediateDirectories: false)
+        } catch let error as NSError {
+            logger.warning("Could not create avatar directory: \(error.localizedDescription)")
+        }
+
+        if !filemanager.changeCurrentDirectoryPath(avatarDir.path) {
+            logger.warning("Could not change into avatar directory")
+            return
+        }
+
+        if filemanager.fileExists(atPath: avatarFile.path) {
+            do {
+                try filemanager.removeItem(atPath: avatarFile.path)
+            } catch let error {
+                logger.warning("Error: \(error.localizedDescription)")
+            }
+        }
+
+        do {
+            try filemanager.copyItem(at: fileUrl, to: avatarFile)
+        } catch let error {
+            logger.warning("Error: \(error.localizedDescription)")
+            return
+        }
+
+        DcConfig.selfavatar = avatarFile.path
+
+    }
+
+}
