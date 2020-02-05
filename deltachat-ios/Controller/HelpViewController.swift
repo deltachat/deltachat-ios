8f9f0131e04e42a4c8e0694745c012281abbe677
nayooti <nayooti@gmail.com>
2020-02-05 17:56:22 +0100

    added webView

--- deltachat-ios/Controller/HelpViewController.swift
+++ deltachat-ios/Controller/HelpViewController.swift
@@ -1,10 +1,41 @@
 import UIKit
+import WebKit
 
 class HelpViewController: UIViewController {
+
+    private lazy var webView: WKWebView = {
+        let view = WKWebView()
+        return view
+    }()
+
+    init() {
+        super.init(nibName: nil, bundle: nil)
+        hidesBottomBarWhenPushed = true
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = String.localized("menu_help")
+        view.backgroundColor = .yellow
+        setupSubviews()
+    }
+
+    private func setupSubviews() {
+        view.addSubview(webView)
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        webView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0).isActive = true
+        if #available(iOS 11, *) {
+            webView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
+        } else {
+            webView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0).isActive = true
+        }
+        webView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0).isActive = true
+        webView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
+
     }
 
 
