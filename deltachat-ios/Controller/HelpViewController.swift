68fc6db1d8450e6f75720478a1e9b93387570104
nayooti <nayooti@gmail.com>
2020-02-06 15:23:25 +0100

    fixed safe layour guide issue and changed bg-color of HelpViewController view

--- deltachat-ios/Controller/HelpViewController.swift
+++ deltachat-ios/Controller/HelpViewController.swift
@@ -20,9 +20,12 @@ class HelpViewController: UIViewController {
     // MARK: - lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+        view.backgroundColor = .white //DcColors.defaultBackgroundColor
         self.title = String.localized("menu_help")
         setupSubviews()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
         loadHtmlContent { [unowned self] url in
             // return to main thread
             DispatchQueue.main.async {
@@ -35,11 +38,18 @@ class HelpViewController: UIViewController {
     private func setupSubviews() {
         view.addSubview(webView)
         webView.translatesAutoresizingMaskIntoConstraints = false
-        webView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0).isActive = true
+        webView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0).isActive = true
+
         if #available(iOS 11, *) {
-            webView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
+            webView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 0).isActive = true
+            webView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: 0).isActive = true
+
+            //webView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
         } else {
-            webView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0).isActive = true
+            webView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 0).isActive = true
+            webView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0).isActive = true
+
+           // webView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0).isActive = true
         }
         webView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0).isActive = true
         webView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
