5bb4ee4a580b5417c019e3d935ea5c0f45bad323
nayooti <nayooti@gmail.com>
2020-03-16 16:24:17 +0100

    added exact search for strings

---
+++ deltachat-ios/ViewModel/ChatListViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  ChatListViewModel.swift
+//  deltachat-ios
+//
+//  Created by Bastian van de Wetering on 16.03.20.
+//  Copyright © 2020 Jonas Reinsch. All rights reserved.
+//
+
+import Foundation
