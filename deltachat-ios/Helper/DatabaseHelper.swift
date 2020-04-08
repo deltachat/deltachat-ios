e2ce6117898461264174dd4e305956403b67902a
cyberta <cyberta@riseup.net>
2020-04-08 18:18:51 +0200

    clean up app group identifier

--- deltachat-ios/Helper/DatabaseHelper.swift
+++ deltachat-ios/Helper/DatabaseHelper.swift
@@ -3,7 +3,7 @@ class DatabaseHelper {
 
     /// The application group identifier defines a group of apps or extensions that have access to a shared container.
     /// The ID is created in the apple developer portal and can be changed there.
-    static let applicationGroupIdentifier = "group.eu.merlinux.group.chat.delta.ios"
+    static let applicationGroupIdentifier = "group.chat.delta.ios"
 
     var sharedDbFile: String {
         guard let fileContainer = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: DatabaseHelper.applicationGroupIdentifier) else {
