0391feb485699e9b286db2e7b5f862c989550f7f
B. Petersen <r10s@b44t.com>
2020-04-11 13:05:31 +0200

    reset applicationIconBadgeNumber on reinstalls
    the applicationIconBadgeNumber is remembered by the system
    on reinstalls (just tested on ios 13.3.1), however, we allow re-installs also
    in-app.
    
    therefore, if the app is unconfigured (which is true for the welcome-screen),
    we have to reset the counter.

--- deltachat-ios/Coordinator/AppCoordinator.swift
+++ deltachat-ios/Coordinator/AppCoordinator.swift
@@ -143,6 +143,11 @@ class AppCoordinator: NSObject, Coordinator {
     }
 
     private func showWelomeController() {
+        // the applicationIconBadgeNumber is remembered by the system even on reinstalls (just tested on ios 13.3.1),
+        // to avoid appearing an old number of a previous installation, we reset the counter manually.
+        // but even when this changes in ios, we need the reset as we allow account-deletion also in-app.
+        UIApplication.shared.applicationIconBadgeNumber = 0
+
         window.rootViewController = welcomeController
         window.makeKeyAndVisible()
     }
