d9af0802164c0071a24440314889be11113f5238
cyberta <cyberta@riseup.net>
2020-04-15 15:46:53 +0200

    proof of concept - sharing of simple text to selfchat

--- DcShare/ShareViewController.swift
+++ DcShare/ShareViewController.swift
@@ -1,24 +1,34 @@
-//
-//  ShareViewController.swift
-//  DcShare
-//
-//  Created by Macci on 15.04.20.
-//  Copyright © 2020 Jonas Reinsch. All rights reserved.
-//
-
 import UIKit
 import Social
+import DcCore
 
 class ShareViewController: SLComposeServiceViewController {
 
+    let dcContext = DcContext.shared
+
+    override func presentationAnimationDidFinish() {
+        let dbHelper = DatabaseHelper()
+        if dbHelper.currentDatabaseLocation == dbHelper.sharedDbFile {
+            dcContext.openDatabase(dbFile: dbHelper.sharedDbFile)
+        } else {
+            cancel()
+        }
+    }
+
     override func isContentValid() -> Bool {
         // Do validation of contentText and/or NSExtensionContext attachments here
-        return true
+        return  !(contentText?.isEmpty ?? true)
     }
 
     override func didSelectPost() {
         // This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
-    
+
+        let selfchatId = dcContext.getChatIdByContactId(contactId: Int(DC_CONTACT_ID_SELF))
+        let message = DcMsg(viewType: DC_MSG_TEXT)
+        message.text = self.contentText
+        message.sendInChat(id: selfchatId)
+
+        dcContext.closeDatabase()
         // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
         self.extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
     }
