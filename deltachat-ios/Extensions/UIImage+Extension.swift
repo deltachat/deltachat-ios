b534141cd3d6199dd1141a8af2185b62abc045f9
cyberta <cyberta@riseup.net>
2020-06-04 17:25:54 +0200

    implement adaptive tint color for hourglass image (#675)

--- deltachat-ios/Extensions/UIImage+Extension.swift
+++ deltachat-ios/Extensions/UIImage+Extension.swift
@@ -12,6 +12,30 @@ extension UIImage {
         return self
     }
 
+     func maskWithColor(color: UIColor) -> UIImage? {
+         let maskImage = cgImage!
+
+         let width = size.width
+         let height = size.height
+         let bounds = CGRect(x: 0, y: 0, width: width, height: height)
+
+         let colorSpace = CGColorSpaceCreateDeviceRGB()
+         let bitmapInfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.premultipliedLast.rawValue)
+         let context = CGContext(data: nil, width: Int(width), height: Int(height), bitsPerComponent: 8, bytesPerRow: 0, space: colorSpace, bitmapInfo: bitmapInfo.rawValue)!
+
+         context.clip(to: bounds, mask: maskImage)
+         context.setFillColor(color.cgColor)
+         context.fill(bounds)
+
+         if let cgImage = context.makeImage() {
+             let coloredImage = UIImage(cgImage: cgImage)
+             return coloredImage
+         } else {
+             return nil
+         }
+     }
+
+
     public convenience init?(color: UIColor, size: CGSize = CGSize(width: 1, height: 1)) {
         let rect = CGRect(origin: .zero, size: size)
         UIGraphicsBeginImageContextWithOptions(rect.size, false, 0.0)
