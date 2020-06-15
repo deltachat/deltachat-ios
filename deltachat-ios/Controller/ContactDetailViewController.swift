319940e5da38b00e2bfde2e944a1aa501f8568d5
cyberta <cyberta@riseup.net>
2020-06-15 16:34:24 +0200

    fix compile error, probably result of a previous merge conflict

--- deltachat-ios/Controller/ContactDetailViewController.swift
+++ deltachat-ios/Controller/ContactDetailViewController.swift
@@ -330,8 +330,10 @@ class ContactDetailViewController: UITableViewController {
     }
 
     private func showGallery() {
-        guard let chatId = viewModel.chatId else { return }
-        let messageIds = viewModel.context.getChatMedia(chatId: chatId, messageType: DC_MSG_IMAGE, messageType2: DC_MSG_GIF, messageType3: DC_MSG_VIDEO)
+        let messageIds = viewModel.context.getChatMedia(chatId: viewModel.chatId,
+                                                        messageType: DC_MSG_IMAGE,
+                                                        messageType2: DC_MSG_GIF,
+                                                        messageType3: DC_MSG_VIDEO)
         let galleryController = GalleryViewController(mediaMessageIds: messageIds)
             navigationController?.pushViewController(galleryController, animated: true)
     }
