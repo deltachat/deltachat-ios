19218a44d84371d282808b3ab3d4c8fe881749c9
cyberta <cyberta@riseup.net>
2020-08-24 16:07:18 +0200

    add custom UITableView to show InputBarAccessoryView correctly

---
+++ deltachat-ios/Chat/Views/ChatTableView.swift
@@ -0,0 +1,26 @@
+import UIKit
+import InputBarAccessoryView
+
+class ChatTableView: UITableView {
+
+    var messageInputBar: InputBarAccessoryView?
+    override var inputAccessoryView: UIView? {
+        return messageInputBar
+    }
+
+
+    override var canBecomeFirstResponder: Bool {
+        return true
+    }
+
+
+    public init(messageInputBar: InputBarAccessoryView?) {
+        self.messageInputBar = messageInputBar
+        super.init(frame: .zero, style: .plain)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
