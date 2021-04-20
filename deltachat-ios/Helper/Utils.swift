091208fb207ed47bdb8c2a8ae644d079d5b12c81
B. Petersen <r10s@b44t.com>
2021-04-21 00:24:20 +0200

    allow local help specified by language+region

--- deltachat-ios/Helper/Utils.swift
+++ deltachat-ios/Helper/Utils.swift
@@ -56,12 +56,4 @@ struct Utils {
         ///TODO: add more file suffixes
         return url.absoluteString.hasSuffix("wav")
     }
-
-    static func getDeviceLanguage() -> String? {
-        // some device languages have suffixes (like en-aus etc.) so we want to cut suffixes off
-        guard let lang = Locale.preferredLanguages.first?.split(separator: "-").first else {
-            return nil
-        }
-        return String(lang)
-    }
 }
