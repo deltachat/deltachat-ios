61de813d940a71e7d09d6f790451d7575f25402c
cyberta <cyberta@riseup.net>
2021-12-04 03:42:29 +0100

    persist background image

--- deltachat-ios/Helper/Constants.swift
+++ deltachat-ios/Helper/Constants.swift
@@ -10,6 +10,7 @@ struct Constants {
         static let deltachatImapEmailKey = "__DELTACHAT_IMAP_EMAIL_KEY__"
         static let deltachatImapPasswordKey = "__DELTACHAT_IMAP_PASSWORD_KEY__"
         static let lastSelectedAccountKey = "__DELTACHAT_LAST_SELECTED_ACCOUNT_KEY__"
+        static let backgroundImageUrl = "__BACKGROUND_IMAGE_URL__"
     }
 
     static let notificationIdentifier = "deltachat-ios-local-notifications"
