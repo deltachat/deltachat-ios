e37fedb5bb51056918a08b59abb4b5dfdb1266cb
cyberta <cyberta@riseup.net>
2022-01-11 13:30:44 +0100

    lint fixes in KeychainManager

--- deltachat-ios/Helper/KeychainManager.swift
+++ deltachat-ios/Helper/KeychainManager.swift
@@ -1,5 +1,3 @@
-
-
 import Foundation
 import Security
 
@@ -21,7 +19,7 @@ public class KeychainManager {
 
     private static func createRandomPassword() -> String {
         let letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXZY1234567890"
-        return String((0..<50).map { _ in letters.randomElement()! })
+        return String((0..<36).map { _ in letters.randomElement()! })
     }
 
     private static func addDBSecret() throws -> String {
@@ -45,7 +43,7 @@ public class KeychainManager {
     }
     
 
-    private static func queryDBSecret() throws -> String  {
+    private static func queryDBSecret() throws -> String {
         let query: [String: Any] = [kSecClass as String: kSecClassGenericPassword,
                                     kSecAttrAccount as String: "dc_db",
                                     kSecMatchLimit as String: kSecMatchLimitOne,
@@ -57,7 +55,7 @@ public class KeychainManager {
         guard status != errSecItemNotFound else { throw KeychainError.noPassword }
         guard status == errSecSuccess else { throw KeychainError.unhandledError(status: status) }
         
-        guard let existingItem = item as? [String : Any],
+        guard let existingItem = item as? [String: Any],
             let passwordData = existingItem[kSecValueData as String] as? Data,
             let password = String(data: passwordData, encoding: String.Encoding.utf8)
         else {
