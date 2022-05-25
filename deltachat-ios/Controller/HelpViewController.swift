12fb79c567633180773e6ca27821fc077626571c
cyberta <cyberta@riseup.net>
2022-05-25 18:17:35 +0200

    allow search in FullMessageView and HelpViewController

--- deltachat-ios/Controller/HelpViewController.swift
+++ deltachat-ios/Controller/HelpViewController.swift
@@ -4,6 +4,15 @@ import DcCore
 
 class HelpViewController: WebViewViewController {
 
+    override init() {
+        super.init()
+        self.allowSearch = true
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = String.localized("menu_help")
