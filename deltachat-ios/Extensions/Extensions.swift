8aa1102bb336b5613a016f5b5d604880e9aec1b6
cyberta <cyberta@riseup.net>
2022-08-22 19:46:18 +0200

    fix lint warning

--- deltachat-ios/Extensions/Extensions.swift
+++ deltachat-ios/Extensions/Extensions.swift
@@ -136,7 +136,7 @@ extension UIScrollView {
 
 extension NSData {
     func sha1() -> String {
-         var digest = [UInt8](repeating: 0, count:Int(CC_SHA1_DIGEST_LENGTH))
+         var digest = [UInt8](repeating: 0, count: Int(CC_SHA1_DIGEST_LENGTH))
          CC_SHA1(bytes, CC_LONG(self.count), &digest)
          let hexBytes = digest.map { String(format: "%02hhx", $0) }
          return hexBytes.joined()
