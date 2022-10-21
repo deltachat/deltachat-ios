4e4fd371f81d69cf0a4de5bac8141f7e33819be2
Simon Laux <mobile.info@simonlaux.de>
2022-10-21 18:01:37 +0200

    add default providers we have in desktop
    they were asked for permission as far as I know.
    
    The checkmark on the custom cell is still a bit bugged,
    it is only shown on opening the page, but not if it was set later,
    hiding it when selecting another cell works,
    I suspect the text field constraints.

--- deltachat-ios/Controller/SettingsVideoChatViewController.swift
+++ deltachat-ios/Controller/SettingsVideoChatViewController.swift
@@ -1,21 +1,50 @@
 import UIKit
 import DcCore
+
+class PredefinedVideoChatOptionCell: UITableViewCell {
+    
+    public var url: String
+    
+    init(label: String, url: String) {
+        self.url = url
+        super.init(style: UITableViewCell.CellStyle.subtitle, reuseIdentifier: label)
+        self.textLabel?.text = label
+        self.detailTextLabel?.text = url
+        self.detailTextLabel?.textColor = .lightGray
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+
 class SettingsVideoChatViewController: UITableViewController {
 
     private var dcContext: DcContext
 
-    private lazy var videoInstanceCell: TextFieldCell = {
-        let cell = TextFieldCell.makeConfigCell(labelID: String.localized("videochat_instance"),
+    private let defaultOptions = [
+        PredefinedVideoChatOptionCell(label: "Jitsi", url: "https://meet.jit.si/$ROOM"),
+        PredefinedVideoChatOptionCell(label: "Systemli", url: "https://meet.systemli.org/$ROOM"),
+        PredefinedVideoChatOptionCell(label: "autistici", url: "https://vc.autistici.org/$ROOM"),
+    ]
+    
+    private lazy var customInstanceCell: TextFieldCell = {
+        let cell = TextFieldCell.makeConfigCell(labelID: String.localized("custom"),
                                                 placeholderID: String.localized("videochat_instance_placeholder"))
         cell.textField.autocapitalizationType = .none
         cell.textField.autocorrectionType = .no
         cell.textField.textContentType = .URL
+        cell.textField.addTarget(self, action: #selector(setCustom), for: .editingDidBegin)
+        cell.textField.addTarget(self, action: #selector(setCustom), for: .valueChanged)
         return cell
     }()
 
     init(dcContext: DcContext) {
         self.dcContext = dcContext
         super.init(style: .grouped)
+        self.updateSelected()
         self.title = String.localized("videochat_instance")
         hidesBottomBarWhenPushed = true
     }
@@ -31,16 +60,64 @@ class SettingsVideoChatViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return self.defaultOptions.count + 1
     }
 
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // selection changed, save value of custom field to ui config
+        dcContext.setConfig("ui.custom_webrtc_instance", customInstanceCell.getText())
+        
+        var newInstance: String?
+        if indexPath.row < self.defaultOptions.count {
+            newInstance = self.defaultOptions[indexPath.row].url
+            self.view.endEditing(true)
+        } else {
+            newInstance = customInstanceCell.getText()
+        }
+        dcContext.setConfig("webrtc_instance", newInstance)
+        
+        updateSelected()
+        
         tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    // called when editing the value of the custom field or clicking on it
+    @objc private func setCustom(_ textField: UITextField) {
+        let newInstance = customInstanceCell.getText()
+        dcContext.setConfig("ui.custom_webrtc_instance", newInstance)
+        dcContext.setConfig("webrtc_instance", newInstance)
+        updateSelected()
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        videoInstanceCell.textField.text = dcContext.getConfig("webrtc_instance")
-        return videoInstanceCell
+        if indexPath.row < self.defaultOptions.count {
+            return self.defaultOptions[indexPath.row]
+        } else {
+            return customInstanceCell
+        }
+    }
+   
+    private var isCustom = true
+    
+    func updateSelected() {
+        let currentUrl = dcContext.getConfig("webrtc_instance")
+        // set selection
+        for option in self.defaultOptions {
+            if option.url == currentUrl {
+                option.accessoryType = .checkmark
+                self.isCustom = false
+            } else {
+                option.accessoryType = .none
+            }
+        }
+        
+        if self.isCustom {
+            customInstanceCell.accessoryType = .checkmark
+            customInstanceCell.textField.text = currentUrl
+        } else {
+            customInstanceCell.accessoryType = .none
+            customInstanceCell.textField.text = dcContext.getConfig("ui.custom_webrtc_instance")
+        }
     }
 
     override func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
@@ -48,6 +125,9 @@ class SettingsVideoChatViewController: UITableViewController {
     }
 
     override func viewWillDisappear(_ animated: Bool) {
-        dcContext.setConfig("webrtc_instance", videoInstanceCell.getText())
+        dcContext.setConfig("ui.custom_webrtc_instance", customInstanceCell.getText())
+        if self.isCustom {
+            dcContext.setConfig("webrtc_instance", customInstanceCell.getText())
+        }
     }
 }
