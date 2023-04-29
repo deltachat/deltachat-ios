65487179439b175a69e95cd90fb0082c6ce2f0fc
B. Petersen <r10s@b44t.com>
2023-04-29 15:22:44 +0200

    call super on override

--- deltachat-ios/Controller/GalleryViewController.swift
+++ deltachat-ios/Controller/GalleryViewController.swift
@@ -94,6 +94,7 @@ class GalleryViewController: UIViewController {
     }
 
     override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         setupContextMenuIfNeeded()
     }
 
