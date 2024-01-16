1a85a82ed511c2ef9c765286b37cc052e96df4ef
Nathan Mattes <hallo@bullenscheisse.de>
2024-01-16 10:38:44 +0100

    Show ... if there are more than five different reactions (#1727)

--- DcCore/DcCore/DC/Wrapper.swift
+++ DcCore/DcCore/DC/Wrapper.swift
@@ -1576,6 +1576,12 @@ public struct DcReaction: Decodable {
     public let count: Int
     public let emoji: String
     public let isFromSelf: Bool
+
+    public init(count: Int = 1, emoji: String, isFromSelf: Bool = false) {
+        self.emoji = emoji
+        self.count = count
+        self.isFromSelf = isFromSelf
+    }
 }
 
 public struct DcReactions: Decodable {
