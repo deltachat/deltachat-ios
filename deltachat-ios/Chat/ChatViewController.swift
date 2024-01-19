7680ad03b19686e733ab1af52a75b4ba73f23933
Nathan Mattes <hallo@bullenscheisse.de>
2024-01-19 13:10:32 +0100

    Color my reactions differently (#1728)

--- deltachat-ios/Chat/ChatViewController.swift
+++ deltachat-ios/Chat/ChatViewController.swift
@@ -1851,7 +1851,9 @@ class ChatViewController: UITableViewController, UITableViewDropDelegate {
               let messageSnapshotView = cell.messageBackgroundContainer.snapshotView(afterScreenUpdates: false)
         else { return nil }
 
-        let sendReactionsView = SendReactionsView(messageId: messageId as String)
+        let myReactions = dcContext.getMessageReactions(messageId: messageId.integerValue)?.reactions.filter { $0.isFromSelf } ?? []
+
+        let sendReactionsView = SendReactionsView(messageId: messageId as String, myReactions: myReactions)
         sendReactionsView.delegate = self
         sendReactionsView.translatesAutoresizingMaskIntoConstraints = false
         sendReactionsView.layoutIfNeeded()
