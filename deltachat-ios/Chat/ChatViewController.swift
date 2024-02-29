99d0d60857764dcc091b995bfd58793c66fe8566
Nathan Mattes <hallo@bullenscheisse.de>
2024-02-29 14:32:44 +0100

    Set so-called message input bar with the embedded contact-request-bar as input-accessory-view
    aka fix the fixed bug :P

--- deltachat-ios/Chat/ChatViewController.swift
+++ deltachat-ios/Chat/ChatViewController.swift
@@ -904,6 +904,7 @@ class ChatViewController: UITableViewController, UITableViewDropDelegate {
         messageInputBar.padding = UIEdgeInsets(top: 6, left: 0, bottom: 6, right: 0)
         messageInputBar.setStackViewItems([], forStack: .top, animated: false)
         messageInputBar.onScrollDownButtonPressed = scrollToBottom
+        inputAccessoryView = messageInputBar
     }
 
     private func configureDraftArea(draft: DraftModel, animated: Bool = true) {
