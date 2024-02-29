d7df1b4afc7450f525edd18870645d37336569d3
Nathan Mattes <hallo@bullenscheisse.de>
2024-02-29 09:49:07 +0100

    Allow selections for readonly-chats (#2088)

--- deltachat-ios/Chat/ChatViewController.swift
+++ deltachat-ios/Chat/ChatViewController.swift
@@ -396,6 +396,8 @@ class ChatViewController: UITableViewController, UITableViewDropDelegate {
         tableView.rowHeight = UITableView.automaticDimension
         tableView.separatorStyle = .none
         tableView.keyboardDismissMode = .interactive
+        tableView.allowsMultipleSelectionDuringEditing = true
+        
         navigationController?.setNavigationBarHidden(false, animated: false)
 
         if #available(iOS 13.0, *) {
@@ -444,7 +446,6 @@ class ChatViewController: UITableViewController, UITableViewDropDelegate {
         draft.parse(draftMsg: dcContext.getDraft(chatId: chatId))
         messageInputBar.inputTextView.text = draft.text
         configureDraftArea(draft: draft, animated: false)
-        tableView.allowsMultipleSelectionDuringEditing = true
         tableView.dragInteractionEnabled = true
         tableView.dropDelegate = self
     }
