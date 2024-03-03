c04fa76f9bbc9a13bcdcef21a734807477baff6e
B. Petersen <r10s@b44t.com>
2024-03-03 17:54:07 +0100

    make notifications discardable, use more reasonable fallback

--- DcNotificationService/NotificationService.swift
+++ DcNotificationService/NotificationService.swift
@@ -1,18 +1,42 @@
 import UserNotifications
+import DcCore
 
 class NotificationService: UNNotificationServiceExtension {
-
+    let dcAccounts = DcAccounts.shared
     var contentHandler: ((UNNotificationContent) -> Void)?
-    var bestAttemptContent: UNMutableNotificationContent?
 
     override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
         self.contentHandler = contentHandler
-        bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
-        
-        if let bestAttemptContent = bestAttemptContent {
-            // Modify the notification content here...
-            bestAttemptContent.title = "\(bestAttemptContent.title) [modified]"
-            
+        guard let bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent) else { return }
+
+        dcAccounts.openDatabase(writeable: false)
+        let eventEmitter = dcAccounts.getEventEmitter()
+        if !dcAccounts.backgroundFetch(timeout: 25) {
+            contentHandler(bestAttemptContent)
+            return
+        }
+
+        var messageCount = 0
+        while true {
+            guard let event = eventEmitter.getNextEvent() else { break }
+            if event.id == DC_EVENT_ACCOUNTS_BACKGROUND_FETCH_DONE { break }
+            if event.id == DC_EVENT_INCOMING_MSG {
+                let dcContext = dcAccounts.get(id: event.accountId)
+                let chat = dcContext.getChat(chatId: event.data1Int)
+                if !UserDefaults.standard.bool(forKey: "notifications_disabled") && !chat.isMuted {
+                    messageCount += 1
+                    let msg = dcContext.getMessage(id: event.data2Int)
+                    let contact = dcContext.getContact(id: msg.fromContactId)
+                    bestAttemptContent.title = chat.isGroup ? chat.name : msg.getSenderName(contact)
+                    bestAttemptContent.body = msg.summary(chars: 80) ?? ""
+                }
+            }
+        }
+
+        if messageCount == 0 {
+            let silentContent = UNMutableNotificationContent()
+            contentHandler(silentContent)
+        } else {
             contentHandler(bestAttemptContent)
         }
     }
@@ -20,9 +44,8 @@ class NotificationService: UNNotificationServiceExtension {
     override func serviceExtensionTimeWillExpire() {
         // Called just before the extension will be terminated by the system.
         // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
-        if let contentHandler = contentHandler, let bestAttemptContent =  bestAttemptContent {
-            contentHandler(bestAttemptContent)
-        }
-    }
 
+        // For Delta Chat, it is just fine to do nothing - assume eg. bad network or mail servers not reachable,
+        // then a "You have new messages" is the best that can be done.
+    }
 }
