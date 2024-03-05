763634808c3641dc0e7010639249d160345eca85
B. Petersen <r10s@b44t.com>
2024-03-05 17:51:17 +0100

    tweak scripts/clean-core.sh
    the idea of the script
    is to prune all possible caches that may cause problems when updating core.
    
    it is run usually before a release and to make sure a core is rebuild from scratch.

--- scripts/clean-core.sh
+++ scripts/clean-core.sh
@@ -1,1 +1,8 @@
+cd deltachat-ios/libraries/deltachat-core-rust
+cargo clean
+cd -
+
 rm -rf deltachat-ios/libraries/deltachat-core-rust/target
+rm deltachat-ios/libraries/libdeltachat.a
+
+echo "now, in Xcode, run 'Product / Clean Build Folder'"
