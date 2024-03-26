241fb67973d14e0f313669697929825bb284d87d
Nathan Mattes <hallo@bullenscheisse.de>
2024-03-26 17:28:50 +0100

    Make scroll to top animatable (#2125)

--- DcCore/DcCore/Extensions/UITableView+Extensions.swift
+++ DcCore/DcCore/Extensions/UITableView+Extensions.swift
@@ -1,14 +1,14 @@
 import UIKit
 
 extension UITableView {
-    public func scrollToTop() {
-        let numberOfSections = self.numberOfSections
-        if numberOfSections > 0 {
-            let numberOfRows = self.numberOfRows(inSection: 0)
-            if numberOfRows > 0 {
-                let indexPath = IndexPath(row: 0, section: 0)
-                self.scrollToRow(at: indexPath, at: UITableView.ScrollPosition.top, animated: false)
-            }
-        }
+  public func scrollToTop(animated: Bool = false) {
+    let numberOfSections = self.numberOfSections
+    if numberOfSections > 0 {
+      let numberOfRows = self.numberOfRows(inSection: 0)
+      if numberOfRows > 0 {
+        let indexPath = IndexPath(row: 0, section: 0)
+        self.scrollToRow(at: indexPath, at: UITableView.ScrollPosition.top, animated: animated)
+      }
     }
+  }
 }
