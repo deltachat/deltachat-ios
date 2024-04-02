437af11bd8ad3580cf082fddefcb01e6cb6cdf5d
Nathan Mattes <hallo@bullenscheisse.de>
2024-04-02 12:56:46 +0200

    Remove dead code (#2111)

--- deltachat-ios/View/Cell/GalleryCell.swift
+++ deltachat-ios/View/Cell/GalleryCell.swift
@@ -70,23 +70,4 @@ class GalleryCell: UICollectionViewCell {
             imageView.alpha = newValue ? 0.75 : 1.0
         }
     }
-
-    @objc func itemDelete(_ sender: Any) {
-        self.performAction(#selector(GalleryCell.itemDelete(_:)), with: sender)
-    }
-
-    @objc func showInChat(_ sender: Any) {
-        self.performAction(#selector(GalleryCell.showInChat(_:)), with: sender)
-    }
-
-    func performAction(_ action: Selector, with sender: Any?) {
-        if let collectionView = self.superview as? UICollectionView, let indexPath = collectionView.indexPath(for: self) {
-            collectionView.delegate?.collectionView?(
-                collectionView,
-                performAction: action,
-                forItemAt: indexPath,
-                withSender: sender
-            )
-        }
-    }
 }
