831252279fbd01609ff83c53966670d9bf1b7fb4
link2xt <link2xt@testrun.org>
2024-04-04 16:42:33 +0200

    Move core building script to a separate file

---
+++ scripts/build-core.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
+export PATH="$HOME/.cargo/bin:$PATH"
+
+FILENAME="libdeltachat.a"
+DIR="../deltachat-ios/libraries"
+
+# Delete old build, if any.
+rm -f "${DIR}/${FILENAME}"
+
+rustc `cat ../rust-toolchain` --version
+
+# ensure all targets are installed
+rustup target add aarch64-apple-ios x86_64-apple-ios --toolchain `cat ../rust-toolchain`
+
+# --xcode-integ determines --release and --targets from Xcode's env vars.
+# Depending your setup, specify the rustup toolchain explicitly.
+RUSTFLAGS="-C lto=on -C embed-bitcode=yes" \
+  cargo +`cat ../rust-toolchain` lipo --xcode-integ --manifest-path "$DIR/deltachat-core-rust/deltachat-ffi/Cargo.toml" --features jsonrpc
+
+# cargo-lipo drops result in different folder, depending on the config.
+if [[ $CONFIGURATION = "Debug" ]]; then
+  SOURCE="$DIR/deltachat-core-rust/target/universal/debug/${FILENAME}"
+else
+  SOURCE="$DIR/deltachat-core-rust/target/universal/release/${FILENAME}"
+fi
+
+# Copy compiled library to DIR.
+if [ -e "${SOURCE}" ]; then
+  cp -a "${SOURCE}" $DIR
+fi
