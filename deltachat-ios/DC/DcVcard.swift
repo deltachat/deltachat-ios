194a27878a5f9d44b0fff9e57e7a498096a5a299
B. Petersen <r10s@b44t.com>
2024-05-29 18:55:48 +0200

    add parseVcard api

---
+++ deltachat-ios/DC/DcVcard.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+public struct DcVcardContact: Decodable {
+    /// Email address.
+    public let addr: String
+
+    /// The contact's name, or the email address if no name was given.
+    public let displayName: String
+
+    /// Public PGP key in Base64.
+    public let key: String?
+
+    /// Profile image in Base64.
+    public let profileImage: String?
+
+    /// Contact color as hex string.
+    public let color: String
+
+    // Last update timestamp.
+    public let timestamp: Int64
+}
+
+struct DcVcardContactResult: Decodable {
+    let result: [DcVcardContact]
+}
