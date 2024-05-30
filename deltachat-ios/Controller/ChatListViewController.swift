4612859edf5de2a11da74a70fbfa22b5aaa989a0
B. Petersen <r10s@b44t.com>
2024-05-30 17:01:32 +0200

    add 1.46 device message

--- deltachat-ios/Controller/ChatListViewController.swift
+++ deltachat-ios/Controller/ChatListViewController.swift
@@ -134,10 +134,10 @@ class ChatListViewController: UITableViewController {
 
         // update messages - for new messages, do not reuse or modify strings but create new ones.
         // it is not needed to keep all past update messages, however, when deleted, also the strings should be deleted.
-        let deviceMsgLabel = "update_1_44c_ios2"
+        let deviceMsgLabel = "update_1_46e_ios"
         if !dcAccounts.isFreshlyAdded(id: dcContext.id) {
             let msg = dcContext.newMessage(viewType: DC_MSG_TEXT)
-            msg.text = String.localizedStringWithFormat(String.localized("update_1_44_ios"), "https://get.delta.chat/#changelogs")
+            msg.text = String.localizedStringWithFormat(String.localized("update_1_46_ios"), "https://get.delta.chat/#changelogs")
             dcContext.addDeviceMessage(label: deviceMsgLabel, msg: msg)
         } else {
             dcContext.addDeviceMessage(label: deviceMsgLabel, msg: nil)
