25612c8910055eceb3e8f03f9c7929ae21117284
Nathan Mattes <hallo@bullenscheisse.de>
2024-06-07 16:29:55 +0200

    Simplify MessageInfo-screen

--- deltachat-ios/Controller/MessageInfoViewController.swift
+++ deltachat-ios/Controller/MessageInfoViewController.swift
@@ -4,11 +4,14 @@ import DcCore
 class MessageInfoViewController: UITableViewController {
     var dcContext: DcContext
     var message: DcMsg
+    private static let reuseIdentifier = "MessageInfoCell"
 
     init(dcContext: DcContext, message: DcMsg) {
         self.dcContext = dcContext
         self.message = message
         super.init(style: .grouped)
+
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: reuseIdentifier)
     }
 
     required init?(coder _: NSCoder) {
@@ -22,21 +25,12 @@ class MessageInfoViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in _: UITableView) -> Int {
-        return 1
-    }
-
     override func tableView(_: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 1 // number of rows in section
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell: UITableViewCell
-        if let c = tableView.dequeueReusableCell(withIdentifier: "MessageInfoCell") {
-            cell = c
-        } else {
-            cell = UITableViewCell(style: .default, reuseIdentifier: "MessageInfoCell")
-        }
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
 
         if indexPath.section == 0 {
             if indexPath.row == 0 {
