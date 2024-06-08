5e0ac87143b8c4953f4abb7e172fa7a92c542eaf
Nathan Mattes <hallo@bullenscheisse.de>
2024-06-08 19:03:11 +0200

    Stage images, but send stickers (#2128)
    `isSticker` with checking on image sizes feels so wrong, if someone has a better idea how to differ between stickers and images: I'm all ears!

--- deltachat-ios/Chat/ChatViewController.swift
+++ deltachat-ios/Chat/ChatViewController.swift
@@ -2666,7 +2666,13 @@ extension ChatViewController: UITextViewDelegate {
 // MARK: - ChatInputTextViewPasteDelegate
 extension ChatViewController: ChatInputTextViewPasteDelegate {
     func onImagePasted(image: UIImage) {
-        sendSticker(image)
+        let isSticker = image.size.equalTo(CGSize(width: 140, height: 140))
+
+        if isSticker {
+            sendSticker(image)
+        } else {
+            stageImage(image)
+        }
     }
 }
 
