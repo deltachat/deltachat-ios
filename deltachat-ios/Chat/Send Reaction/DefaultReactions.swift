dd3f01fdb4e9bd91bf7c9afe8c8345202587597f
Nathan Mattes <hallo@bullenscheisse.de>
2024-07-21 10:29:02 +0200

    Consider all five default-emojis (#2112)

--- deltachat-ios/Chat/Send Reaction/DefaultReactions.swift
+++ deltachat-ios/Chat/Send Reaction/DefaultReactions.swift
@@ -5,12 +5,16 @@ enum DefaultReactions: CaseIterable {
     case thumbsUp
     case thumbsDown
     case heart
+    case faceWithTearsOfJoy
+    case sad
 
     var emoji: String {
         switch self {
         case .thumbsUp: return "👍"
         case .thumbsDown: return "👎"
         case .heart: return "❤️"
+        case .faceWithTearsOfJoy: return "😂"
+        case .sad: return "🙁"
         }
     }
 }
