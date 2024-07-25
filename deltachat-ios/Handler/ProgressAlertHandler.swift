b0ba11c5458a2f4571d39716257b6ce4bdddec0f
Nathan Mattes <hallo@bullenscheisse.de>
2024-07-25 16:05:01 +0200

    Migrate Instant-Onboarding to use non-legacy ProgressAlertHandler

--- deltachat-ios/Handler/ProgressAlertHandler.swift
+++ deltachat-ios/Handler/ProgressAlertHandler.swift
@@ -8,11 +8,14 @@ protocol ProgressAlertHandlerDataSource: AnyObject {
 class ProgressAlertHandler {
 
     let dcAccounts: DcAccounts
-    weak var dataSource: ProgressAlertHandlerDataSource
+    weak var dataSource: ProgressAlertHandlerDataSource?
+    var onSuccess: (() -> Void)?
+    private var progressAlertController: UIAlertController?
 
     // add Completion block
-    init(dcAccounts: DcAccounts, notification: Notification.Name) {
+    init(dcAccounts: DcAccounts, notification: Notification.Name, onSuccess: (() -> Void)? = nil) {
         self.dcAccounts = dcAccounts
+        self.onSuccess = onSuccess
 
         NotificationCenter.default.addObserver(self, selector: #selector(Self.handleNotification(_:)), name: notification, object: nil)
     }
@@ -25,9 +28,68 @@ class ProgressAlertHandler {
             self.updateProgressAlert(error: ui["errorMessage"] as? String)
         } else if ui["done"] as? Bool ?? false {
             dcAccounts.startIo()
-//            self.updateProgressAlertSuccess(completion: onSuccess)
+            self.updateProgressAlertSuccess(completion: onSuccess)
         } else {
-//            self.updateProgressAlertValue(value: ui["progress"] as? Int)
+            self.updateProgressAlertValue(value: ui["progress"] as? Int)
         }
+    }
+
+    public func updateProgressAlertValue(value: Int?) {
+        guard let value else { return }
+        guard let progressAlertController else { return assertionFailure("Please present an alert") }
+
+        progressAlertController.message = String.localized("one_moment") + " " + String(value/10) + "%"
+    }
+
+    public func updateProgressAlert(message: String) {
+        guard let progressAlertController else { return assertionFailure("Please present an alert") }
+
+        progressAlertController.message = message
+    }
+
+    public func updateProgressAlert(error message: String?, completion onComplete: (() -> Void)? = nil) {
+        guard let progressAlertController else { return assertionFailure("Please present an alert") }
+        guard let dataSource else { return assertionFailure("No DataSource") }
+
+        // CAVE: show the new alert in the dismiss-done-handler of the previous one -
+        // otherwise we get the error "Attempt to present <UIAlertController: ...> while a presentation is in progress."
+        // and the error won't be shown.
+        // (when animated is true, that works also sequentially, however better not rely on that, also we do not want an animation here)
+        progressAlertController.dismiss(animated: false) {
+            let errorAlert = UIAlertController(title: String.localized("error"), message: message, preferredStyle: .alert)
+            errorAlert.addAction(UIAlertAction(title: String.localized("ok"), style: .default, handler: { _ in
+                onComplete?()
+            }))
+            // sometimes error messages are not shown and we get the same error as above
+            // as a workaround we disable animated here as well
+            dataSource.viewController().present(errorAlert, animated: false, completion: nil)
+        }
+    }
+
+    public func updateProgressAlertSuccess(completion onComplete: VoidFunction?) {        
+        guard let progressAlertController else { return assertionFailure("Please present an alert") }
+
+        updateProgressAlertValue(value: 1000)
+        // delay so the user has time to read the success message
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1, execute: {
+            progressAlertController.dismiss(animated: true) {
+                onComplete?()
+            }
+        })
+    }
+
+    public func showProgressAlert(title: String, dcContext: DcContext) {
+        guard let dataSource else { return assertionFailure("No DataSource") }
+
+        let progressAlertController = UIAlertController(title: title, message: String.localized("one_moment"), preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: String.localized("cancel"),
+                                         style: .cancel,
+                                         handler: { _ in
+            dcContext.stopOngoingProcess()
+        })
+        progressAlertController.addAction(cancelAction)
+
+        dataSource.viewController().present(progressAlertController, animated: true, completion: nil)
+        self.progressAlertController = progressAlertController
     }
 }
