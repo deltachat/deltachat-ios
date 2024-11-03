bfbb1281f46873f625d5161d9e3f38b64006f3f4
B. Petersen <r10s@b44t.com>
2024-11-03 20:53:25 +0100

    fix forward-icon for iOS 15

--- deltachat-ios/Helper/Menu/LegacyMenuItem.swift
+++ deltachat-ios/Helper/Menu/LegacyMenuItem.swift
@@ -14,9 +14,13 @@ class LegacyMenuItem: UIMenuItem {
 @available(iOS 13.0, *)
 extension UIAction {
     static func menuAction(localizationKey: String, attributes: UIAction.Attributes = [], systemImageName: String, indexPath: IndexPath, action: @escaping (IndexPath) -> Void) -> UIAction {
+        return menuAction(localizationKey: localizationKey, attributes: attributes, image: UIImage(systemName: systemImageName), indexPath: indexPath, action: action)
+    }
+
+    static func menuAction(localizationKey: String, attributes: UIAction.Attributes = [], image: UIImage?, indexPath: IndexPath, action: @escaping (IndexPath) -> Void) -> UIAction {
         return UIAction(
             title: String.localized(localizationKey),
-            image: UIImage(systemName: systemImageName),
+            image: image,
             attributes: attributes,
             handler: { _ in
                 DispatchQueue.main.async {
