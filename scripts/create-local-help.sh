a3ea1df38cac4445bda5a9de0621808131e7c807
B. Petersen <r10s@b44t.com>
2024-11-27 20:43:00 +0100

    adapt to changed deltachat-pages output folder

--- scripts/create-local-help.sh
+++ scripts/create-local-help.sh
@@ -1,4 +1,4 @@
-../deltachat-pages/tools/create-local-help.py ../deltachat-pages/_site deltachat-ios/Assets/Help --add-top-links
+../deltachat-pages/tools/create-local-help.py ../deltachat-pages/result deltachat-ios/Assets/Help --add-top-links
 
 rm -r deltachat-ios/Assets/Help/zh-Hant
 mv deltachat-ios/Assets/Help/zh_CN deltachat-ios/Assets/Help/zh-Hant
