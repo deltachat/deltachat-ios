44a011e046bd80692d49be06965d87468e251b58
Nathan Mattes <hallo@bullenscheisse.de>
2024-11-27 12:29:42 +0100

    Add WidgetExtension with Default-implementation (#2406)

---
+++ MostRecentWebXDCWidget/MostRecentWebXDCWidgetControl.swift
@@ -0,0 +1,47 @@
+import AppIntents
+import SwiftUI
+import WidgetKit
+
+struct MostRecentWebXDCWidgetControl: ControlWidget {
+    var body: some ControlWidgetConfiguration {
+        StaticControlConfiguration(
+            kind: "chat.delta.MostRecentWebXDCWidget",
+            provider: Provider()
+        ) { value in
+            ControlWidgetToggle(
+                "Start Timer",
+                isOn: value,
+                action: StartTimerIntent()
+            ) { isRunning in
+                Label(isRunning ? "On" : "Off", systemImage: "timer")
+            }
+        }
+        .displayName("Timer")
+        .description("A an example control that runs a timer.")
+    }
+}
+
+extension MostRecentWebXDCWidgetControl {
+    struct Provider: ControlValueProvider {
+        var previewValue: Bool {
+            false
+        }
+
+        func currentValue() async throws -> Bool {
+            let isRunning = true // Check if the timer is running
+            return isRunning
+        }
+    }
+}
+
+struct StartTimerIntent: SetValueIntent {
+    static let title: LocalizedStringResource = "Start a timer"
+
+    @Parameter(title: "Timer is running")
+    var value: Bool
+
+    func perform() async throws -> some IntentResult {
+        // Start / stop the timer based on `value`.
+        return .result()
+    }
+}
