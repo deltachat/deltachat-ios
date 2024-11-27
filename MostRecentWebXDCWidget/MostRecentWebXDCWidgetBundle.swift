d0a71d873fbbf83b437c16999777ac0266e4065f
Nathan Mattes <hallo@bullenscheisse.de>
2024-11-27 12:29:42 +0100

    Add WidgetExtension with Default-implementation (#2406)

---
+++ MostRecentWebXDCWidget/MostRecentWebXDCWidgetBundle.swift
@@ -0,0 +1,10 @@
+import WidgetKit
+import SwiftUI
+
+@main
+struct MostRecentWebXDCWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        MostRecentWebXDCWidget()
+        MostRecentWebXDCWidgetControl()
+    }
+}
