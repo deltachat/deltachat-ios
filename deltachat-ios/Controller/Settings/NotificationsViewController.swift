00e1bd8b0654102b2c270f15fff5f4a5549eaa9c
B. Petersen <r10s@b44t.com>
2024-12-08 00:37:36 +0100

    move notification toggle to ViewController

---
+++ deltachat-ios/Controller/Settings/NotificationsViewController.swift
@@ -0,0 +1,89 @@
+import UIKit
+import DcCore
+import Intents
+
+internal final class NotificationsViewController: UITableViewController {
+
+    private struct SectionConfigs {
+        let headerTitle: String?
+        let footerTitle: String?
+        let cells: [UITableViewCell]
+    }
+
+    private enum CellTags: Int {
+        case notifications
+    }
+
+    private var dcContext: DcContext
+    internal let dcAccounts: DcAccounts
+
+    // MARK: - cells
+    private lazy var notificationsCell: SwitchCell = {
+        return SwitchCell(
+            textLabel: String.localized("pref_notifications"),
+            on: !dcContext.isMuted(),
+            action: { [weak self] cell in
+                guard let self else { return }
+
+                dcContext.setMuted(!cell.isOn)
+                if cell.isOn {
+                    if let appDelegate = UIApplication.shared.delegate as? AppDelegate {
+                        appDelegate.registerForNotifications()
+                    }
+                } else {
+                    NotificationManager.removeAllNotifications()
+                }
+
+                NotificationManager.updateBadgeCounters()
+                NotificationCenter.default.post(name: Event.messagesChanged, object: nil, userInfo: ["message_id": Int(0), "chat_id": Int(0)])
+        })
+    }()
+
+    private lazy var sections: [SectionConfigs] = {
+        let preferencesSection = SectionConfigs(
+            headerTitle: nil,
+            footerTitle: nil,
+            cells: [notificationsCell]
+        )
+        return [preferencesSection]
+    }()
+
+    init(dcAccounts: DcAccounts) {
+        self.dcContext = dcAccounts.getSelected()
+        self.dcAccounts = dcAccounts
+        super.init(style: .grouped)
+        hidesBottomBarWhenPushed = true
+    }
+
+    required init?(coder _: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = String.localized("pref_notifications")
+        tableView.rowHeight = UITableView.automaticDimension
+    }
+
+    // MARK: - UITableViewDelegate + UITableViewDatasource
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return sections.count
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sections[section].cells.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return sections[indexPath.section].cells[indexPath.row]
+    }
+
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sections[section].headerTitle
+    }
+
+    override func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
+        return sections[section].footerTitle
+    }
+}
