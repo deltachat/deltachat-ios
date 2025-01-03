fbfcb76c34eeb2b8aa8c61f110d9136a45ec08b6
Nathan Mattes <hallo@bullenscheisse.de>
2025-01-03 12:10:48 +0100

    Remove LegacyItem (#2453)

--- deltachat-ios/Extensions/UIAction+Extension.swift
+++ deltachat-ios/Extensions/UIAction+Extension.swift
@@ -1,16 +1,5 @@
 import UIKit
 
-@available(*, deprecated)
-class LegacyMenuItem: UIMenuItem {
-    var indexPath: IndexPath?
-
-    convenience init(title: String, action: Selector, indexPath: IndexPath?) {
-        self.init(title: title, action: action)
-
-        self.indexPath = indexPath
-    }
-}
-
 extension UIAction {
     static func menuAction(localizationKey: String, attributes: UIAction.Attributes = [], systemImageName: String, indexPath: IndexPath, action: @escaping (IndexPath) -> Void) -> UIAction {
         return menuAction(localizationKey: localizationKey, attributes: attributes, image: UIImage(systemName: systemImageName), indexPath: indexPath, action: action)
