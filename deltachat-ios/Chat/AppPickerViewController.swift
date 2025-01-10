f72c898be27f3a7a52012d94356966a726ae0794
Nathan Mattes <hallo@bullenscheisse.de>
2025-01-10 12:31:58 +0100

    Add basic webview to show store (#2450)

--- deltachat-ios/Chat/AppPickerViewController.swift
+++ deltachat-ios/Chat/AppPickerViewController.swift
@@ -9,16 +9,43 @@ class AppPickerViewController: UIViewController {
     // Web view
     // Context
     weak var delegate: AppPickerViewControllerDelegate?
+    let webView: WKWebView
 
-    init() {
+    init(url: URL = URL(string: "https://webxdc.org/apps/")!) {
+        webView = WKWebView(frame: .zero)
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        webView.load(URLRequest(url: url))
+
         super.init(nibName: nil, bundle: nil)
+
+        webView.navigationDelegate = self
+        view.addSubview(webView)
         view.backgroundColor = .systemGroupedBackground
+        setupConstraints()
+
         title = String.localized("webxdc_apps")
     }
 
     required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+
+    private func setupConstraints() {
+        let constraints = [
+            webView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            view.trailingAnchor.constraint(equalTo: webView.trailingAnchor),
+            view.bottomAnchor.constraint(equalTo: webView.bottomAnchor),
+        ]
+
+        NSLayoutConstraint.activate(constraints)
+    }
 }
 
 extension AppPickerViewController: WKNavigationDelegate {
-    // intercept download, store in core and call delegate
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping @MainActor (WKNavigationActionPolicy) -> Void) {
+        decisionHandler(.allow)
+
+        // if url ends with .xdc -> download and store in core and call delegate
+        // else if host = webxdc -> allow
+        // else -> open in browser and .deny
+    }
 }
