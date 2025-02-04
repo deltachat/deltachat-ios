fb77922cd5918db288ec468b0cf250de3a071842
B. Petersen <r10s@b44t.com>
2025-02-04 13:30:35 +0100

    left-align buttons, make them ready for adding icons

--- deltachat-ios/View/ActionCell.swift
+++ deltachat-ios/View/ActionCell.swift
@@ -8,45 +8,22 @@ class ActionCell: UITableViewCell {
 
     var actionTitle: String? {
         didSet {
-            actionLabel.text = actionTitle
+            textLabel?.text = actionTitle
         }
     }
 
     var actionColor: UIColor? {
         didSet {
-            actionLabel.textColor = actionColor ?? UIColor.systemBlue
+            textLabel?.textColor = actionColor ?? UIColor.systemBlue
         }
     }
 
-    private lazy var actionLabel: UILabel = {
-        let label = UILabel()
-        label.text = actionTitle
-        label.textColor = UIColor.systemBlue
-        label.font = .preferredFont(forTextStyle: .body)
-        label.adjustsFontForContentSizeCategory = true
-        label.textAlignment = .center
-        label.numberOfLines = 0
-        return label
-    }()
-
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        setupSubviews()
+        textLabel?.textColor = UIColor.systemBlue
     }
 
     required init?(coder _: NSCoder) {
         fatalError("init(coder:) has not been implemented")
-    }
-
-    private func setupSubviews() {
-        contentView.addSubview(actionLabel)
-        actionLabel.translatesAutoresizingMaskIntoConstraints = false
-        contentView.addConstraints([
-            actionLabel.constraintAlignLeadingTo(contentView, paddingLeading: 12),
-            actionLabel.constraintAlignTrailingTo(contentView, paddingTrailing: 12),
-            actionLabel.constraintAlignTopTo(contentView, paddingTop: 12),
-            actionLabel.constraintAlignBottomTo(contentView, paddingBottom: 12)
-
-        ])
     }
 }
