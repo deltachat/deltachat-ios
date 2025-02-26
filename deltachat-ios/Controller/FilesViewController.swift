4487c3f61e0472e5bc1059fdf26bbc59c3e3fd9b
B. Petersen <r10s@b44t.com>
2025-02-26 13:09:35 +0100

    update translations

--- deltachat-ios/Controller/FilesViewController.swift
+++ deltachat-ios/Controller/FilesViewController.swift
@@ -132,7 +132,7 @@ class FilesViewController: UIViewController {
     // MARK: - actions
 
     private func askToDeleteItem(at indexPath: IndexPath) {
-        let title = String.localized(stringID: "ask_delete_messages_simple", parameter: 1)
+        let title = String.localized(stringID: "ask_delete_messages", parameter: 1)
         let alertController =  UIAlertController(title: title, message: nil, preferredStyle: .safeActionSheet)
         let okAction = UIAlertAction(title: String.localized("delete"), style: .destructive, handler: { [weak self] _ in
             self?.deleteItem(at: indexPath)
