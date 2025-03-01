eb42bbe1f5605fac4131a7e211512490756e8ded
B. Petersen <r10s@b44t.com>
2025-03-01 22:13:48 +0100

    show keyboard when start editing message text

--- deltachat-ios/Chat/ChatViewController.swift
+++ deltachat-ios/Chat/ChatViewController.swift
@@ -1696,6 +1696,7 @@ class ChatViewController: UITableViewController, UITableViewDropDelegate {
         draft.sendEditRequestFor = message.id
         configureDraftArea(draft: draft)
         messageInputBar.inputTextView.text = message.text
+        focusInputTextView()
     }
 
     private func toggleSave(at indexPath: IndexPath) {
