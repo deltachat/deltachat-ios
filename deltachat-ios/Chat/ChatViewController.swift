b0379844d324c7ef338891e91d707a512d12869c
B. Petersen <r10s@b44t.com>
2025-03-07 21:41:25 +0100

    add 'call' icon to one-to-one chats

--- deltachat-ios/Chat/ChatViewController.swift
+++ deltachat-ios/Chat/ChatViewController.swift
@@ -883,6 +883,11 @@ class ChatViewController: UITableViewController, UITableViewDropDelegate {
                 let recentlySeen = DcUtils.showRecentlySeen(context: dcContext, chat: dcChat)
                 initialsBadge.setRecentlySeen(recentlySeen)
                 rightBarButtonItems.append(badgeItem)
+
+                if !dcChat.isGroup && dcChat.canSend, let config = dcContext.getConfig("webrtc_instance"), !config.isEmpty {
+                    let button = UIBarButtonItem(image: UIImage(systemName: "phone"), style: .plain, target: self, action: #selector(callPressed))
+                    rightBarButtonItems.append(button)
+                }
             } else {
                 let button = UIBarButtonItem(image: UIImage(systemName: "magnifyingglass"), style: .plain, target: self, action: #selector(searchPressed))
                 rightBarButtonItems.append(button)
@@ -1171,6 +1176,9 @@ class ChatViewController: UITableViewController, UITableViewDropDelegate {
         DispatchQueue.main.async { [weak self] in
             self?.searchController.isActive = true
         }
+    }
+
+    @objc private func callPressed() {
     }
 
     private func clipperButtonMenu() -> UIMenu {
