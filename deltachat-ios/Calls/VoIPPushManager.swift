17613cd41c175b4f290c41e8f7faca46b1e65423
B. Petersen <r10s@b44t.com>
2025-03-14 13:35:01 +0100

    make calls more visible in log

--- deltachat-ios/Calls/VoIPPushManager.swift
+++ deltachat-ios/Calls/VoIPPushManager.swift
@@ -15,12 +15,12 @@ class VoIPPushManager: NSObject, PKPushRegistryDelegate {
 
     func pushRegistry(_ registry: PKPushRegistry, didUpdate pushCredentials: PKPushCredentials, for type: PKPushType) {
         // VoIP tokens are not used
-        logger.info("VoIP token received")
+        logger.info("☎️ voIP token received")
     }
 
     func pushRegistry(_ registry: PKPushRegistry, didReceiveIncomingPushWith payload: PKPushPayload, for type: PKPushType) {
         let callInfo = payload.dictionaryPayload
-        logger.info("didReceiveIncomingPushWith: \(callInfo)")
+        logger.info("☎️ didReceiveIncomingPushWith: \(callInfo)")
         guard let accountId = callInfo["account_id"] as? Int,
               let msgId = callInfo["message_id"] as? Int else { return }
         CallManager.shared.reportIncomingCall(accountId: accountId, msgId: msgId)
