bc4c2ca5561e1816de16961cd880ae1c08b114aa
B. Petersen <r10s@b44t.com>
2025-03-14 15:50:47 +0100

    more logging

--- deltachat-ios/DC/DcContext.swift
+++ deltachat-ios/DC/DcContext.swift
@@ -794,14 +794,17 @@ public class DcContext {
 
     public func placeOutgoingCall(chatId: Int) -> Int {
         let msgId = dc_place_outgoing_call(contextPointer, UInt32(chatId))
+        logger.info("☎️ (\(self.id),\(msgId))=dc_place_outgoing_call(\(chatId)")
         return Int(msgId)
     }
 
     public func acceptIncomingCall(msgId: Int) {
+        logger.info("☎️ dc_accept_incoming_call(\(self.id),\(msgId))")
         dc_accept_incoming_call(contextPointer, UInt32(msgId))
     }
 
     public func endCall(msgId: Int) {
+        logger.info("☎️ dc_end_call(\(self.id),\(msgId))")
         dc_end_call(contextPointer, UInt32(msgId))
     }
 }
