226cfaa767f04b6df73807afeb352afbab977e81
B. Petersen <r10s@b44t.com>
2025-03-14 13:35:01 +0100

    make calls more visible in log

--- DcNotificationService/NotificationService.swift
+++ DcNotificationService/NotificationService.swift
@@ -84,7 +84,7 @@ class NotificationService: UNNotificationServiceExtension {
                     notifications.append(content)
                 }
             } else if event.id == DC_EVENT_INCOMING_CALL {
-                UserDefaults.pushToDebugArray("📞")
+                UserDefaults.pushToDebugArray("☎️")
                 let dcContext = dcAccounts.get(id: event.accountId)
                 let msg = dcContext.getMessage(id: event.data1Int)
 
@@ -95,7 +95,7 @@ class NotificationService: UNNotificationServiceExtension {
                     ]
                     // calling reportNewIncomingVoIPPushPayload ends up in didReceiveIncomingPushWith in the main app
                     CXProvider.reportNewIncomingVoIPPushPayload(payload) { error in
-                        if let error = error {
+                        if let error {
                             UserDefaults.pushToDebugArray("ERR6 " + error.localizedDescription)
                         } else {
                             UserDefaults.pushToDebugArray("OK2")
