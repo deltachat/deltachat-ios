2b558bbaef993c0cb83faa75aac653f7c688cce3
Casper Zandbergen <info@casperzandbergen.nl>
2025-04-14 12:57:43 +0200

    Allow pasting gifs

--- deltachat-ios/Extensions/UIPasteboard.swift
+++ deltachat-ios/Extensions/UIPasteboard.swift
@@ -5,11 +5,19 @@ import UniformTypeIdentifiers
 extension UIPasteboard {
     /// Also returns true for webp
     public var hasImagesExtended: Bool {
-        return hasImages || types.contains(UTType.webP.identifier)
+        hasImages || types.contains(UTType.webP.identifier)
     }
 
-    /// Also returns webp image
+    /// Also supports gif and webp using SDAnimatedImage and SDWebImage
     public var imageExtended: UIImage? {
-        return image ?? UIImage.sd_image(withWebPData: data(forPasteboardType: UTType.webP.identifier))
+        gif ?? image ?? webp
+    }
+
+    private var gif: SDAnimatedImage? {
+        data(forPasteboardType: UTType.gif.identifier).flatMap(SDAnimatedImage.init(data:))
+    }
+
+    private var webp: UIImage? {
+        data(forPasteboardType: UTType.webP.identifier).flatMap(UIImage.sd_image(withWebPData:))
     }
 }
