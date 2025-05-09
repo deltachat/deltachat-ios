e84a10141daf80c38a92919d0207770aff120dba
B. Petersen <r10s@b44t.com>
2025-05-09 17:28:13 +0200

    tweak paddings

--- deltachat-ios/View/ContactDetailHeader.swift
+++ deltachat-ios/View/ContactDetailHeader.swift
@@ -5,10 +5,10 @@ class ContactDetailHeader: UIStackView {
 
     var onAvatarTap: VoidFunction?
 
-    public static let headerHeight: CGFloat = 230
+    public static let headerHeight: CGFloat = 240
 
     private lazy var avatar: InitialsBadge = {
-        let badge = InitialsBadge(size: 150)
+        let badge = InitialsBadge(size: 160)
         badge.setColor(UIColor.lightGray)
         badge.isAccessibilityElement = false
         badge.translatesAutoresizingMaskIntoConstraints = false
@@ -21,8 +21,12 @@ class ContactDetailHeader: UIStackView {
         let label = UILabel()
         label.lineBreakMode = .byTruncatingTail
         label.textColor = DcColors.defaultTextColor
-        label.adjustsFontForContentSizeCategory = true
-        label.font = .preferredFont(forTextStyle: UIFont.TextStyle.title1)
+
+        let baseFont = UIFont.preferredFont(forTextStyle: .title1)
+        if let descriptor = baseFont.fontDescriptor.withSymbolicTraits(.traitBold) {
+            label.font = UIFont(descriptor: descriptor, size: descriptor.pointSize)
+        }
+
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
@@ -39,9 +43,10 @@ class ContactDetailHeader: UIStackView {
     }()
 
     private(set) lazy var titleLabelContainer: UIStackView = {
-        let stackView = UIStackView(arrangedSubviews: [titleLabel, greenCheckmark])
+        let stackView = UIStackView(arrangedSubviews: [UIView(), titleLabel, greenCheckmark, UIView()])
         stackView.axis = .horizontal
-        stackView.spacing = 4
+        stackView.spacing = 8
+        stackView.alignment = .center
         stackView.translatesAutoresizingMaskIntoConstraints = false
         return stackView
     }()
@@ -51,8 +56,17 @@ class ContactDetailHeader: UIStackView {
         backgroundColor = .clear
         axis = .vertical
         alignment = .center
+        spacing = 12
+
+        addArrangedSubview(UIView()) // spacer
         addArrangedSubview(avatar)
         addArrangedSubview(titleLabelContainer)
+        addArrangedSubview(UIView()) // spacer
+
+        addConstraints([
+            greenCheckmark.constraintHeightTo(titleLabel.font.pointSize * 0.8),
+            greenCheckmark.widthAnchor.constraint(equalTo: greenCheckmark.heightAnchor),
+        ])
     }
 
     required init(coder: NSCoder) {
