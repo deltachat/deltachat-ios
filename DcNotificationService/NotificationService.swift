4e19c1a9ee263805f88383719a1092208156a5ed
Casper Zandbergen <info@casperzandbergen.nl>
2025-05-10 17:13:30 +0200

    prevent "You have new messages" spam

--- DcNotificationService/NotificationService.swift
+++ DcNotificationService/NotificationService.swift
@@ -26,7 +26,8 @@ class NotificationService: UNNotificationServiceExtension {
         // by the system soon and any notification is better than nothing.
         var exitedDueToCriticalMemory = false
         let memoryPressureSource = DispatchSource.makeMemoryPressureSource(eventMask: .critical)
-        memoryPressureSource.setEventHandler {
+        memoryPressureSource.setEventHandler { [weak memoryPressureSource] in
+            guard let memoryPressureSource, !memoryPressureSource.isCancelled else { return }
             memoryPressureSource.cancel()
             // Order of importance because we might crash very soon
             exitedDueToCriticalMemory = true
