039434e8f99c4c041cf4ff7e3448c77a95af4d09
Casper Zandbergen <info@casperzandbergen.nl>
2025-06-28 12:43:19 +0200

    only answer notification when io is running

--- deltachat-ios/AppDelegate.swift
+++ deltachat-ios/AppDelegate.swift
@@ -35,6 +35,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
     /// Other processes like the Notification Service Extension can post
     /// DarwinNotification.appRunningQuestion in which case we reply with .appRunningConfirmation
     @objc func appRunningQuestion(notification: DarwinNotification) {
+        guard UserDefaults.mainIoRunning else { return }
         DarwinNotificationCenter.current.post(.appRunningConfirmation)
     }
 
