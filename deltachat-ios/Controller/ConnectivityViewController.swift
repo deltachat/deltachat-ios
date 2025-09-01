9bd7a0e9d422bee96ae138cb215b6b13931110a5
B. Petersen <r10s@b44t.com>
2025-09-02 01:20:42 +0200

    remove notification state from connectivity

--- deltachat-ios/Controller/ConnectivityViewController.swift
+++ deltachat-ios/Controller/ConnectivityViewController.swift
@@ -4,20 +4,12 @@ import Network
 
 class ConnectivityViewController: WebViewViewController {
 
-    private var connectivityMonitor: NWPathMonitor?
-    private var isLowDataMode: Bool = false
-
     override init(dcContext: DcContext) {
         super.init(dcContext: dcContext)
 
         // set connectivity changed observer before we actually init html,
         // otherwise, we may miss events and the html is not correct.
         NotificationCenter.default.addObserver(self, selector: #selector(ConnectivityViewController.handleConnectivityChanged(_:)), name: Event.connectivityChanged, object: nil)
-        NotificationCenter.default.addObserver(self,
-                                               selector: #selector(ConnectivityViewController.handleLowerPowerModeChanged(_:)),
-                                               name: NSNotification.Name.NSProcessInfoPowerStateDidChange,
-                                               object: nil
-        )
     }
     
     required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
@@ -30,130 +22,11 @@ class ConnectivityViewController: WebViewViewController {
         self.webView.isOpaque = false
         self.webView.backgroundColor = .clear
         view.backgroundColor = DcColors.defaultBackgroundColor
-
-        let connectivityMonitor = NWPathMonitor()
-        connectivityMonitor.pathUpdateHandler = { [weak self] path in
-            guard let self else { return }
-            self.isLowDataMode = path.isConstrained
-            self.loadHtml()
-        }
-        isLowDataMode = connectivityMonitor.currentPath.isConstrained
-        connectivityMonitor.start(queue: DispatchQueue.global())
-        self.connectivityMonitor = connectivityMonitor
     }
     
     // called everytime the view will appear
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        loadHtml()
-    }
-
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        connectivityMonitor?.cancel()
-    }
-
-    // this method needs to be run from a background thread
-    private func getNotificationStatus(backgroundRefreshStatus: UIBackgroundRefreshStatus) -> String {
-        let connectiviy = self.dcContext.getConnectivity()
-        let pushState = dcContext.getPushState()
-        let title = " <b>" + String.localized("pref_notifications") + ":</b> "
-        let notificationsEnabledInDC = !dcContext.isMuted()
-        var notificationsEnabledInSystem = false
-        let semaphore = DispatchSemaphore(value: 0)
-        DispatchQueue.global(qos: .userInitiated).async {
-            NotificationManager.notificationEnabledInSystem { enabled in
-                notificationsEnabledInSystem = enabled
-                semaphore.signal()
-            }
-        }
-        if semaphore.wait(timeout: .now() + 1) == .timedOut {
-            return "<span class=\"red dot\"></span>"
-                .appending(title)
-                .appending("Timeout Error")
-        }
-
-        if dcContext.isAnyDatabaseEncrypted() {
-            return "<span class=\"red dot\"></span>"
-                .appending(title)
-                .appending("Unreliable due to \"Encrypted Accounts\" experiment, see \"Device Messages\" for fixing")
-        }
-
-        if !notificationsEnabledInDC {
-            return "<span class=\"disabled dot\"></span>"
-                .appending(title)
-                .appending(String.localized("disabled_in_dc"))
-        }
-
-        if !notificationsEnabledInSystem {
-            return "<span class=\"disabled dot\"></span>"
-                .appending(title)
-                .appending(String.localized("disabled_in_system_settings"))
-        }
-
-        if backgroundRefreshStatus != .available {
-            return "<span class=\"disabled dot\"></span>"
-                .appending(title)
-                .appending(String.localized("bg_app_refresh_disabled"))
-        }
-
-        if pushState == DC_PUSH_NOT_CONNECTED || connectiviy == DC_CONNECTIVITY_NOT_CONNECTED {
-            return "<span class=\"red dot\"></span>"
-                .appending(title)
-                .appending(String.localized("connectivity_not_connected"))
-        }
-
-        if isLowDataMode {
-            return "<span class=\"disabled dot\"></span>"
-                .appending(title)
-                .appending(String.localized("connectivity_low_data_mode"))
-        }
-
-        if ProcessInfo.processInfo.isLowPowerModeEnabled {
-            return "<span class=\"disabled dot\"></span>"
-                .appending(title)
-                .appending(String.localized("connectivity_low_power_mode"))
-        }
-
-        if pushState == DC_PUSH_CONNECTED {
-            return "<span class=\"green dot\"></span>"
-                .appending(title)
-                .appending(String.localized("connectivity_connected"))
-        }
-
-        let timestamps = UserDefaults.standard.array(forKey: Constants.Keys.notificationTimestamps) as? [Double]
-        guard let timestamps = timestamps, !timestamps.isEmpty else {
-            // in most cases, here the app was just installed and we do not have any data.
-            // so, do not show something error-like here.
-            // (in case of errors, it usually converts to an error sooner or later)
-            return "<span class=\"yellow dot\"></span>"
-                .appending(title)
-                .appending(String.localized("connectivity_connected"))
-        }
-
-        let averageDelta = (Double(Date().timeIntervalSince1970) - timestamps.first!) / Double(timestamps.count)
-
-        var lastWakeups = ""
-        var lastWakeupsCnt = 0
-        for timestamp in timestamps.reversed() {
-            lastWakeups += (lastWakeupsCnt > 0 ? ", " : "") + DateUtils.getExtendedAbsTimeSpanString(timeStamp: timestamp)
-            lastWakeupsCnt += 1
-            if lastWakeupsCnt >= 3 {
-                break
-            }
-        }
-
-        return  "<span class=\"yellow dot\"></span>"
-            .appending(title)
-            .appending(String.localizedStringWithFormat(String.localized("last_check_at"), lastWakeups))
-            .appending(", ")
-            .appending(averageDelta / 3600 > 2 ?
-                       String.localized(stringID: "notifications_avg_hours", parameter: Int(averageDelta / 3600)) :
-                       String.localized(stringID: "notifications_avg_minutes", parameter: Int(averageDelta / 60)))
-    }
-
-
-    @objc private func handleLowerPowerModeChanged(_ notification: Notification) {
         loadHtml()
     }
 
@@ -164,45 +37,33 @@ class ConnectivityViewController: WebViewViewController {
     }
 
     private func loadHtml() {
-        DispatchQueue.main.async { [weak self] in
+        DispatchQueue.global(qos: .userInitiated).async { [weak self] in
             guard let self else { return }
-            // `UIApplication.shared` needs to be called from main thread
-            let backgroundRefreshStatus = UIApplication.shared.backgroundRefreshStatus
+            let html = dcContext.getConnectivityHtml()
+                .replacingOccurrences(of: "</style>", with:
+                    """
+                    body {
+                        font-size: 13pt;
+                        font-family: -apple-system, sans-serif;
+                        padding: 0 .5rem .5rem .5rem;
+                        -webkit-text-size-adjust: none;
+                    }
 
-            // do the remaining things in background thread
-            DispatchQueue.global(qos: .userInitiated).async { [weak self] in
-                guard let self else { return }
-                var html = self.dcContext.getConnectivityHtml()
-                    .replacingOccurrences(of: "</style>", with:
-                        """
-                        body {
-                            font-size: 13pt;
-                            font-family: -apple-system, sans-serif;
-                            padding: 0 .5rem .5rem .5rem;
-                            -webkit-text-size-adjust: none;
-                        }
+                    .disabled {
+                        background-color: #aaaaaa;
+                    }
 
-                        .disabled {
-                            background-color: #aaaaaa;
-                        }
+                    @media (prefers-color-scheme: dark) {
+                      body {
+                        background-color: black !important;
+                        color: #eee;
+                      }
+                    }
+                    </style>
+                    """)
 
-                        @media (prefers-color-scheme: dark) {
-                          body {
-                            background-color: black !important;
-                            color: #eee;
-                          }
-                        }
-                        </style>
-                        """)
-
-                let notificationStatus = self.getNotificationStatus(backgroundRefreshStatus: backgroundRefreshStatus)
-                if let range = html.range(of: "</ul>") {
-                    html = html.replacingCharacters(in: range, with: "<li>" + notificationStatus + "</li></ul>")
-                }
-
-                DispatchQueue.main.async {
-                    self.webView.loadHTMLString(html, baseURL: nil)
-                }
+            DispatchQueue.main.async { [weak self] in
+                self?.webView.loadHTMLString(html, baseURL: nil)
             }
         }
     }
