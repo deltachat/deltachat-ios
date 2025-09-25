597a08624621a5f04ba5c88c6f4a595a3a900ecc
Casper Zandbergen <info@casperzandbergen.nl>
2025-09-25 13:33:42 +0200

    Fixed sharing screenshots

--- DcCore/DcCore/Helper/CodableNSItemProvider.swift
+++ DcCore/DcCore/Helper/CodableNSItemProvider.swift
@@ -27,11 +27,11 @@ public enum CodableNSItemProvider: Codable {
     public init(from provider: NSItemProvider) async throws {
         self = try await withCheckedThrowingContinuation { continuation in
             switch provider.hasItemConformingToTypeIdentifier {
-            case UTType.gif.identifier: loadFileURL(forType: .gif, DC_MSG_GIF)
-            case UTType.webP.identifier: loadFileURL(forType: .webP, DC_MSG_IMAGE)
-            case UTType.png.identifier: loadFileURL(forType: .png, DC_MSG_IMAGE)
-            case UTType.jpeg.identifier: loadFileURL(forType: .jpeg, DC_MSG_IMAGE)
-            case UTType.image.identifier: loadFileURL(forType: .image, DC_MSG_IMAGE)
+            case UTType.gif.identifier: loadFileURL(forType: .gif, DC_MSG_GIF, plistToImage: true)
+            case UTType.webP.identifier: loadFileURL(forType: .webP, DC_MSG_IMAGE, plistToImage: true)
+            case UTType.png.identifier: loadFileURL(forType: .png, DC_MSG_IMAGE, plistToImage: true)
+            case UTType.jpeg.identifier: loadFileURL(forType: .jpeg, DC_MSG_IMAGE, plistToImage: true)
+            case UTType.image.identifier: loadFileURL(forType: .image, DC_MSG_IMAGE, plistToImage: true)
             case UTType.mpeg4Movie.identifier: loadFileURL(forType: .mpeg4Movie, DC_MSG_VIDEO)
             case UTType.quickTimeMovie.identifier: loadFileURL(forType: .quickTimeMovie, DC_MSG_VIDEO)
             case UTType.movie.identifier: loadFileURL(forType: .movie, DC_MSG_VIDEO)
@@ -41,22 +41,33 @@ public enum CodableNSItemProvider: Codable {
             case UTType.item.identifier: loadFileURL(forType: .item, DC_MSG_FILE)
             default: continuation.resume(throwing: Error.unknownType)
             }
-            func loadFileURL(forType type: UTType, _ viewType: Int32) {
-                provider.loadInPlaceFileRepresentation(forTypeIdentifier: type.identifier) { url, _, error in
+            func loadFileURL(forType type: UTType, _ viewType: Int32, plistToImage: Bool = false) {
+                provider.loadFileRepresentation(forTypeIdentifier: type.identifier) { url, error in
                     guard let url else {
                         return continuation.resume(throwing: error ?? Error.providerDidNotReturnValueNorError)
                     }
-                    NSFileCoordinator().coordinate(readingItemAt: url, error: nil) { url in
-                        do {
-                            let tempFile = shareExtensionDirectory
-                                .appendingPathComponent(UUID().uuidString)
-                                .appendingPathExtension(url.pathExtension)
+                    let tempFile = shareExtensionDirectory
+                        .appendingPathComponent(UUID().uuidString)
+                        .appendingPathExtension(url.pathExtension)
+                    do {
+                        // If an app shares a UIImage instead of a path (eg the native screenshot process)
+                        // the file we receive is a PList so we need to load the UIImage differently.
+                        if plistToImage, url.isBplist() {
+                            let data = try Data(contentsOf: url)
+                            let image = try NSKeyedUnarchiver.unarchivedObject(ofClass: UIImage.self, from: data)
+                            if let pngData = image?.pngData() {
+                                try pngData.write(to: tempFile)
+                                continuation.resume(returning: .contentsAt(url: tempFile, viewType: viewType))
+                            } else {
+                                throw Error.loadingImageFailed
+                            }
+                        } else {
                             try FileManager.default.copyItem(at: url, to: tempFile)
                             continuation.resume(returning: .contentsAt(url: tempFile, viewType: viewType))
-                        } catch {
-                            logger.error("Failed to copy file with error \(error)")
-                            continuation.resume(throwing: error)
                         }
+                    } catch {
+                        logger.error("Failed to copy file with error \(error)")
+                        continuation.resume(throwing: error)
                     }
                 }
             }
@@ -91,6 +102,7 @@ public enum CodableNSItemProvider: Codable {
     enum Error: Swift.Error {
         case unknownType
         case failedToConvertDataToString
+        case loadingImageFailed
         /// Should never be called
         case providerDidNotReturnValueNorError
     }
@@ -99,4 +111,12 @@ public enum CodableNSItemProvider: Codable {
 /// Helper for using switch with function where each case is sent as argument into the function
 private func ~= <T>(value: T, block: (T) -> Bool) -> Bool {
     block(value)
+}
+
+private extension URL {
+    func isBplist() -> Bool {
+        guard let handle = try? FileHandle(forReadingFrom: self) else { return false }
+        let data = handle.readData(ofLength: 6)
+        return data == Data("bplist".utf8)
+    }
 }
