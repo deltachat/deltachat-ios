8e913d436d32c8a4ec1fb2b2f4159b7f9f7252b1
B. Petersen <r10s@b44t.com>
2025-11-13 15:17:36 +0100

    'top links' are no longer needed, we have proper navigation now

--- scripts/create-local-help.sh
+++ scripts/create-local-help.sh
@@ -1,5 +1,5 @@
 rm deltachat-ios/Assets/Help/*.png
-../deltachat-pages/tools/create-local-help.py ../deltachat-pages/result deltachat-ios/Assets/Help --add_top_links
+../deltachat-pages/tools/create-local-help.py ../deltachat-pages/result deltachat-ios/Assets/Help
 
 rm -r deltachat-ios/Assets/Help/zh-Hant
 mv deltachat-ios/Assets/Help/zh_CN deltachat-ios/Assets/Help/zh-Hant
