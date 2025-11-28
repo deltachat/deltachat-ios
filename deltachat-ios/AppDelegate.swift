e577b7567a365aa5c065512fc9e80c3567ad84a5
B. Petersen <r10s@b44t.com>
2025-11-28 13:49:09 +0100

    reset delete_server_after for chatmail

--- deltachat-ios/AppDelegate.swift
+++ deltachat-ios/AppDelegate.swift
@@ -115,6 +115,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
                     logger.error("\(error)")
                 }
             }
+
+            // migration 2025-11-28: needed until core starts ignoring "delete_server_after" for chatmail or drops the setting at all
+            if dcContext.isChatmail {
+                dcContext.setConfig("delete_server_after", nil) // reset - let core decide based on bcc_self aka "Multi-Transport Mode"
+            }
+            // /migration 2025-11-28
         }
 
         if dcAccounts.getAll().isEmpty, dcAccounts.add() == 0 {
