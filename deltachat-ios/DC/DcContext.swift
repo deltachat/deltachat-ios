db1b1046fc8427e9aca43e0ae68bae6cb9c79dba
Casper Zandbergen <info@casperzandbergen.nl>
2026-02-05 16:50:05 +0100

    fixed another rebase mistake

--- deltachat-ios/DC/DcContext.swift
+++ deltachat-ios/DC/DcContext.swift
@@ -773,8 +773,8 @@ public class DcContext {
         setConfig("proxy_url", allProxies)
     }
 
-    public func placeOutgoingCall(chatId: Int, placeCallInfo: String, hasVideo: Bool) -> Int {
-        let msgId = dc_place_outgoing_call(contextPointer, UInt32(chatId), placeCallInfo, hasVideo ? 1 : 0)
+    public func placeOutgoingCall(chatId: Int, placeCallInfo: String, hasVideoInitially: Bool) -> Int {
+        let msgId = dc_place_outgoing_call(contextPointer, UInt32(chatId), placeCallInfo, hasVideoInitially ? 1 : 0)
         logger.info("☎️ (\(self.id),\(msgId))=dc_place_outgoing_call(\(chatId)")
         return Int(msgId)
     }
