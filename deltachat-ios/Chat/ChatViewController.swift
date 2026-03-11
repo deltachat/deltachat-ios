cf08abb3687ced5439ecbabc8b76300e10cb794d
B. Petersen <r10s@b44t.com>
2026-03-11 12:02:22 +0100

    unify wording

--- deltachat-ios/Chat/ChatViewController.swift
+++ deltachat-ios/Chat/ChatViewController.swift
@@ -1328,7 +1328,7 @@ class ChatViewController: UIViewController, UITableViewDelegate, UITableViewData
     private func askToDeleteChat() {
         let chat = dcContext.getChat(chatId: chatId)
         let title = String.localizedStringWithFormat(String.localized("ask_delete_named_chat"), chat.name)
-        confirmationAlert(title: title, actionTitle: String.localized("delete"), actionStyle: .destructive,
+        confirmationAlert(title: title, actionTitle: String.localized("delete_for_me"), actionStyle: .destructive,
                           actionHandler: { [weak self] _ in
             guard let self else { return }
             self.dcContext.deleteReferencesAndChat(chatId: self.chatId)
