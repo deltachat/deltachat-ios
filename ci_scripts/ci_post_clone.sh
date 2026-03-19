c5eeb494ef35f110866875cb9ed9176ae205604c
Casper Zandbergen <info@casperzandbergen.nl>
2026-03-19 18:48:58 +0100

    rustup will install cargo

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -3,15 +3,15 @@
 # Install [rustup](https://rustup.rs)
 brew install rustup
 
+# Make sure the correct rust version is installed
+rustup toolchain install `cat rust-toolchain`
+
 # Install [cargo-lipo](https://github.com/TimNN/cargo-lipo#installation)
 cargo install cargo-lipo
 
 # Note: CocoaPods is pre-installed on Xcode Cloud
 
-# Setup workspace
+# Download chatmail/core
 git submodule update --init --recursive
-
-# Make sure the correct rust version is installed
-rustup toolchain install `cat rust-toolchain`
 
 pod install
