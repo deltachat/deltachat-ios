88d1878c85ca6c2d82f7ee63ad1af3f924ee277a
Casper Zandbergen <info@casperzandbergen.nl>
2026-03-19 19:33:12 +0100

    Update ci_post_clone.sh

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -5,16 +5,19 @@ set -euxo pipefail
 # Go to the project folder
 cd ..
 
+export RUSTUP_DIST_SERVER=https://mirrors.ustc.edu.cn/rust-static
+export RUSTUP_UPDATE_ROOT=https://mirrors.ustc.edu.cn/rust-static/rustup
 # Install [rustup](https://rustup.rs)
-brew install -q rustup
+curl https://sh.rustup.rs -sSf | sh -s -- -y
 
 # Make sure the correct rust version is installed
 rustup toolchain install `cat rust-toolchain`
 rustup default $(cat rust-toolchain)
+
 source $HOME/.cargo/env
 
 # Install [cargo-lipo](https://github.com/TimNN/cargo-lipo#installation)
-rustup run cargo install cargo-lipo
+cargo install cargo-lipo
 
 # Note: CocoaPods is pre-installed on Xcode Cloud
 
