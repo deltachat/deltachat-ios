c20661579572db5d3a158953a1eec124a835f985
Casper Zandbergen <info@casperzandbergen.nl>
2026-05-16 10:44:32 +0200

    start removing unnecessary code

--- deltachat-ios/Chat/Views/ChatInputTextView.swift
+++ deltachat-ios/Chat/Views/ChatInputTextView.swift
@@ -2,16 +2,12 @@ import Foundation
 import UIKit
 import MobileCoreServices
 
-public class ChatInputTextView: InputTextView {
+public protocol ChatInputTextViewPasteDelegate: AnyObject {
+    func onImagePasted(_ image: UIImage)
+}
 
+public class ChatInputTextView: UITextView {
     public weak var imagePasteDelegate: ChatInputTextViewPasteDelegate?
-    private lazy var dropInteraction: ChatDropInteraction = {
-        return ChatDropInteraction()
-    }()
-
-    public func setDropInteractionDelegate(delegate: ChatDropInteractionDelegate) {
-        dropInteraction.delegate = delegate
-    }
 
     // MARK: - Image Paste Support
     open override func canPerformAction(_ action: Selector, withSender sender: Any?) -> Bool {
@@ -25,24 +21,7 @@ public class ChatInputTextView: InputTextView {
         guard let image = UIPasteboard.general.imageExtended else {
             return super.paste(sender)
         }
-        imagePasteDelegate?.onImagePasted(image: image)
+        imagePasteDelegate?.onImagePasted(image)
     }
 }
 
-extension ChatInputTextView: UIDropInteractionDelegate {
-    public func dropInteraction(_ interaction: UIDropInteraction, canHandle session: UIDropSession) -> Bool {
-        return dropInteraction.dropInteraction(canHandle: session)
-    }
-
-    public func dropInteraction(_ interaction: UIDropInteraction, sessionDidUpdate session: UIDropSession) -> UIDropProposal {
-        return dropInteraction.dropInteraction(sessionDidUpdate: session)
-    }
-
-    public func dropInteraction(_ interaction: UIDropInteraction, performDrop session: UIDropSession) {
-        dropInteraction.dropInteraction(performDrop: session)
-    }
-}
-
-public protocol ChatInputTextViewPasteDelegate: AnyObject {
-    func onImagePasted(image: UIImage)
-}
