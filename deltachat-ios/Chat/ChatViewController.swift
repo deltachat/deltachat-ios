eeefa52b32190f7e1a4bbdc74962b21b2e871d37
Casper Zandbergen <info@casperzandbergen.nl>
2026-06-13 08:07:09 +0200

    Attempt to fix misalignments on pre liquid glass

--- deltachat-ios/Chat/ChatViewController.swift
+++ deltachat-ios/Chat/ChatViewController.swift
@@ -2801,7 +2801,7 @@ struct InputBarView: View {
     var updateIntrinsicContentSize: () -> Void
 
     var buttonSize: CGFloat {
-        isLiquidGlassEnabled ? 54 : 40
+        isLiquidGlassEnabled ? 54 : 36
     }
 
     var body: some View {
@@ -2850,7 +2850,7 @@ struct InputBarView: View {
                     }
                     .padding(.horizontal, 10)
                     .modifier { glassEffect(view: $0, minHeight: buttonSize, interactive: true) }
-                    .frame(maxHeight: 150)
+                    .frame(maxHeight: 150, alignment: .center)
                     .onTapGesture {
                         textEditorFocus = true
                     }
