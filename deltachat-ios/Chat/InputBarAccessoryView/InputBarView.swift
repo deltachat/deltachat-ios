1b76d5d4a0ae6f6ccb34bd7346e38209d2b2afe6
Casper Zandbergen <info@casperzandbergen.nl>
2026-07-09 09:37:31 +0200

    match toolbar appearance by adding divider and padding like UIToolbar

--- deltachat-ios/Chat/InputBarAccessoryView/InputBarView.swift
+++ deltachat-ios/Chat/InputBarAccessoryView/InputBarView.swift
@@ -110,6 +110,8 @@ struct InputBarView: View {
                 }
             } else {
                 view.background(Material.bar, ignoresSafeAreaEdges: .bottom)
+                    .overlay(alignment: .top, content: Divider.init)
+                    .padding(.top, 8)
             }
         }
     }
