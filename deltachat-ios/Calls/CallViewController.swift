57401846bec14452b246d9f3b185886af80f637f
Casper Zandbergen <info@casperzandbergen.nl>
2026-09-12 20:56:44 +0200

    remove todos that are fixed in this PR

--- deltachat-ios/Calls/CallViewController.swift
+++ deltachat-ios/Calls/CallViewController.swift
@@ -3,9 +3,7 @@ import UIKit
 import WebRTC
 
 // TODO: Minimize call to PiP when app is opened from a deeplink (or from a notification)
-// TODO: Fix missed call logic: if the missed call was from me dont send notification
 // TODO: Actually stop capturing mic when muted
-// FIXME: Still doesn't always work when in background
 
 class CallViewController: UIViewController {
     private enum CallStatus: Equatable {
