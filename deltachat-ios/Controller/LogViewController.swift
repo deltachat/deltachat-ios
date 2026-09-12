48e7a85d4d03e31e32ab64a0cbffee9cafe36306
B. Petersen <r10s@b44t.com>
2026-09-12 22:24:29 +0200

    use assign/if combination

--- deltachat-ios/Controller/LogViewController.swift
+++ deltachat-ios/Controller/LogViewController.swift
@@ -83,12 +83,11 @@ public class LogViewController: UIViewController {
         info += "any-database-encrypted=\(dcContext.isAnyDatabaseEncrypted())\n"
 
         if let appDelegate = UIApplication.shared.delegate as? AppDelegate {
-            let notifyTokenBegin: String
-            if let notifiyToken = appDelegate.notifyToken {
+            let notifyTokenBegin = if let notifiyToken = appDelegate.notifyToken {
                 // half of the notifiy token is unusally enough for debugging
-                notifyTokenBegin = String(notifiyToken.prefix(notifiyToken.count/2)) + "..."
+                String(notifiyToken.prefix(notifiyToken.count/2)) + "..."
             } else {
-                notifyTokenBegin = "<unset>"
+                "<unset>"
             }
             info += "notify-token=\(notifyTokenBegin)\n"
         }
