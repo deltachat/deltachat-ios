a4e0e31048e0fc3316801dadfe002f8cafb78a7b
Casper Zandbergen <info@casperzandbergen.nl>
2026-09-16 20:00:23 +0200

    fix wrong chat content inset on appear

--- deltachat-ios/Chat/ChatViewController.swift
+++ deltachat-ios/Chat/ChatViewController.swift
@@ -382,6 +382,22 @@ class ChatViewController: UIViewController, UITableViewDelegate, UITableViewData
         navigationItem.backButtonTitle = String.localized("chat")
         definesPresentationContext = true
 
+        if #available(iOS 27, *) {
+            // On iOS 27 the tabbar of the previous view causes a second viewSafeAreaInsetsDidChange
+            // when it hides which happens during layout of the toolbar so the inset calculation is
+            // wrong on initial appear. To fix this we just recalculate the insets every time the
+            // toolbar height changes.
+            toolbarContainerView
+                .publisher(for: \.bounds)
+                .map(\.height)
+                .receive(on: RunLoop.main) // This is necessary because bounds might change right before shouldProcessContentInsetUpdates is updated eg from scrolling
+                .sink { [weak self] _ in
+                    guard self?.shouldProcessContentInsetUpdates == true else { return }
+                    self?.setTableViewContentInset()
+                }
+                .store(in: &bag)
+        }
+
         let animateKeyboardChange: KeyboardManager.EventCallback = { [weak self, tableView] notification in
             guard let self, shouldProcessContentInsetUpdates else { return }
             // Using superview instead of window here because in iOS 13+ a modal can change
